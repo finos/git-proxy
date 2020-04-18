353f71bfde508142d17c72a62faf4c71a23e79f2
pGrovesy <paul.timothy.groves@gmail.com>
2020-04-18 14:45:03 +0100

    work in progress

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const proxy = require('express-http-proxy');
-const fs = require('fs');
 const app = require('express')();
 const https = require('https');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const httpPort = 3000;
 const httpsPort = 3001;
 
+const options = {
+  inflate: true,
+  limit: '2048kb',
+  type: '*/*',
+};
+
+app.use(bodyParser.raw(options));
+
 app.use('/', proxy('https://github.com', {
   filter: function(req, res) {
-    const message = JSON.stringify(req.headers);
-    console.log(message);
+    const headers = JSON.stringify(req.headers);
+    const body = req.body;
+    console.log(body);
+
+    if (body && body.length) {
+      req.rawBody = body.toString('utf8');
+    }
+
+    console.log(req.originalUrl);
+    console.log(headers);
+    console.log(`body=${req.rawBody}`);
+    return true;
   },
 }));
 
