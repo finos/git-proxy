5c6a579329bb01e84d10573b075b43eaf8a973be
pGrovesy <paul.timothy.groves@gmail.com>
2020-04-18 12:41:31 +0100

    working in progress

--- index.js
+++ index.js
@@ -2,22 +2,23 @@ const proxy = require('express-http-proxy');
 const fs = require('fs');
 const app = require('express')();
 const https = require('https');
-const port = 8080;
-
+const httpPort = 3000;
+const httpsPort = 3001;
 
 app.use('/', proxy('https://github.com', {
   filter: function(req, res) {
-    console.log('hello');
-    return req.method === 'GET';
+    const message = JSON.stringify(req.headers);
+    console.log(message);
   },
 }));
 
+app.listen(httpPort, () => {
+  console.log(`Listening on ${httpPort}`);
+});
 
 https.createServer({
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.cert'),
-}, app).listen(port, function() {
-  console.log(`Example app listening on port ${port} Go to https://localhost:${port}/`);
+}, app).listen(httpsPort, function() {
+  console.log(`Listening on ${httpsPort}`);
 });
-
-
