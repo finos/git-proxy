0d89f358c29597cf2e8a836c5f40a6ee14683fa7
pGrovesy <paul.timothy.groves@gmail.com>
2020-04-20 14:45:39 +0100

    adding gulp tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,49 @@
+const log = require('why-is-node-running');
 const gulp = require('gulp');
+
 const mocha = require('gulp-mocha');
-const livereload = require('gulp-livereload');
+
 
 const watchList = ['index.js', 'test/**', 'lib/**'];
-const tests = 'test/*.js';
+const testFiles = ['test/**/*.js'];
 
-exports.test = () => (
-  gulp.src(['test/**/*.js'], {read: false})
-      .pipe(mocha({reporter: 'list', exit: true}))
-      .on('error', console.error));
+let node = null;
 
-exports.watch = () => {
-  livereload.listen();
+const server = function(cb) {
+  const spawn = require('child_process').spawn;
+  if (node) {
+    node.kill();
+  }
+
+  node = spawn('node', ['index.js'], {stdio: 'inherit'});
+  cb();
+
+  node.on('close', (code) => {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  });
+};
+
+const watchTests = function() {
   gulp.watch(watchList, () => {
     return gulp.src(tests)
-        .pipe(livereload())
         .pipe(mocha({reporter: 'list'}))
         .on('error', (err) => {
           console.log(err.stack);
         });
   });
 };
+
+exports.server = server;
+exports.start = gulp.series(server, test);
+exports.watchTest = watchTests;
+exports.watchServer = gulp.watch(watchList, server);
+
+
+// clean up if an error goes unhandled.
+process.on('exit', function() {
+  if (node) {
+    node.kill();
+  }
+});
