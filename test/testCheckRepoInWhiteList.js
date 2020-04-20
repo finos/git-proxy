db80c396ce952da8a99b4ff39aa8ac467a01e077
pGrovesy <paul.timothy.groves@gmail.com>
2020-04-20 14:45:39 +0100

    adding gulp tests

--- test/testCheckRepoInWhiteList.js
+++ test/testCheckRepoInWhiteList.js
@@ -1,17 +1,13 @@
 const assert = require('assert');
 
 describe('Array', () => {
-  describe('#indexOf()', function() {
+  describe('#indexOf()', () => {
     it('should return -1 when the value is not present', (done) => {
       assert.equal([1, 2, 3].indexOf(4), -1);
       done();
     });
-  });
-});
 
-describe('Array', () => {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', (done) => {
+    it('should return -2 when the value is not present', (done) => {
       assert.equal([1, 2, 3].indexOf(4), -1);
       done();
     });
