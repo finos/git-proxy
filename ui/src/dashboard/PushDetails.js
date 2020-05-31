56d36d18b09b9856f96277c03e462b2c1e3f7c5d
grovesy <62599442+grovesy@users.noreply.github.com>
2020-05-31 18:47:40 +0100

    work in progress

--- ui/src/dashboard/PushDetails.js
+++ ui/src/dashboard/PushDetails.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { useParams } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+
 import Title from './Title';
 import Card from './Card';
 import { Divider } from '@material-ui/core';
@@ -44,9 +47,23 @@ export default function PushDetails() {
     const url = `http://localhost:8080/api/v1/pushes/${id}`;
     await fetch(url)
       .then(res => res.json())
-      .then(res => {
-        console.log(res)
+      .then(res => {              
+        res.diff = res.steps.find(x => x.stepName === 'diff');
+        console.log(res);
         setPush(res)
+
+      })
+      .catch(err => setErrors(err));
+  }
+
+  async function authorise() {
+    const url = `http://localhost:8080/api/v1/auth/${id}`;
+    console.log(url)
+    await fetch(url)
+      .then(x => x.json())
+      .then(x => {              
+        console.log("AUTH");
+        console.log(x);
       })
       .catch(err => setErrors(err));
   }
@@ -57,35 +74,43 @@ export default function PushDetails() {
 
   if (push.length === 0) {
     return <div><h3>Loading</h3></div>
-  } else {
-    
-    return (
-
-      <React.Fragment>
-        <Title>Push Details {id}</Title>
-        <Divider />
-        <br/>
-        <Grid container spacing={1}>
-          <Grid item xs={2} md={2} lg={2}>
-            <Card title="Repository" text={push.repoName}/>                      
-          </Grid>
-          <Grid item xs={2} md={2} lg={2}>
-            <Card title="Branch" text={push.branch.replace('refs/heads/', '')}/>
-          </Grid>
-          <Grid item xs={4} md={4} lg={4}>
-            <Card title="From" text={push.commit.substring(8)}/>
-          </Grid>
-
-          <Grid item xs={4} md={4} lg={4}>
-            <Card title="To" text={push.commit2.substring(8)}/>
-          </Grid>          
-              
-        
-          <Grid item xs={12} md={12} lg={12}>
-            <Diff diff={push.actionLog[3].logs}/>
-          </Grid>
-        </Grid>
-      </React.Fragment>
-    );
   }
+    
+  return (
+
+    <React.Fragment>
+      <Title>Push Details</Title>
+      <Divider />
+      <br/>
+      <Grid container spacing={1}>
+        <Grid item xs={3} md={3} lg={3}>
+          <Card title="Repository" text={push.repoName}/>                      
+        </Grid>
+        <Grid item xs={3} md={3} lg={3}>
+          <Card title="Repository" text={push.project}/>                      
+        </Grid>          
+        <Grid item xs={6} md={6} lg={6}>
+          <Card title="Branch" text={push.branch.replace('refs/heads/', '')}/>
+        </Grid>
+
+
+
+        <Grid item xs={6} md={6} lg={6}>
+          <Card title="From" text={push.commitTo.substring(8)}/>
+        </Grid>
+
+        <Grid item xs={6} md={6} lg={6}>
+          <Card title="To" text={push.commitFrom.substring(8)}/>
+        </Grid>          
+            
+        <Grid item xs={12} md={12} lg={12}>
+          <Button onClick={authorise}>Authorise</Button>
+        </Grid>
+
+        <Grid item xs={12} md={12} lg={12}>
+          <Diff diff={push.diff.content}/>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 }
