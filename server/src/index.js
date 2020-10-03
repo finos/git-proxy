bcb3d06a3ab493b700015e2b05502c9d2a550840
grovesy <62599442+grovesy@users.noreply.github.com>
2020-10-03 14:32:46 +0100

    material UI added

--- server/src/index.js
+++
@@ -1,5 +0,0 @@
-const config = require('config')
-const db = require('./db')
-
-module.exports.config = config;
-module.exports.db = db
---
+++ src/index.js
@@ -0,0 +1,40 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.9.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+// core components
+import Admin from "ui/layouts/Admin.js";
+import RTL from "ui/layouts/RTL.js";
+
+import "ui/assets/css/material-dashboard-react.css?v=1.9.0";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/admin" component={Admin} />
+      <Route path="/rtl" component={RTL} />
+      <Redirect from="/" to="/admin/dashboard" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
