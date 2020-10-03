8ea99d33eecbbc1b3da8197222fb4381a6b5fd06
grovesy <62599442+grovesy@users.noreply.github.com>
2020-10-03 16:00:33 +0100

    adding login page

--- src/routes.js
+++ src/routes.js
@@ -21,6 +21,7 @@ import Person from "@material-ui/icons/Person";
 
 import DashboardPage from "ui/views/Dashboard/Dashboard.js";
 import OpenPushRequests from "ui/views/OpenPushRequests/OpenPushRequests.js";
+import Login from "ui/views/Login/Login.js";
 
 // import LibraryBooks from "@material-ui/icons/LibraryBooks";
 // import BubbleChart from "@material-ui/icons/BubbleChart";
@@ -47,6 +48,13 @@ const dashboardRoutes = [
     component: OpenPushRequests,
     layout: "/admin"
   },
+  {
+    path: "/login",
+    name: "Login",    
+    icon: Person,
+    component: Login,
+    layout: "/admin"
+  },  
 ];
 
 export default dashboardRoutes;
