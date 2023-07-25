531091ebc4b3c7214af58927491fc776f9cbae88
vaibssingh <vaibhavsingh592.vs@gmail.com>
2023-07-25 23:09:03 +0530

    feat: linting fixes

--- src/ui/assets/jss/material-dashboard-react.js
+++ src/ui/assets/jss/material-dashboard-react.js
@@ -232,6 +232,7 @@ const title = {
   marginTop: '30px',
   marginBottom: '25px',
   minHeight: '32px',
+  // eslint-disable-next-line quotes
   fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
   '& small': {
     color: grayColor[1],
--- src/ui/assets/jss/material-dashboard-react/views/dashboardStyle.js
+++ src/ui/assets/jss/material-dashboard-react/views/dashboardStyle.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 import {
   successColor,
   whiteColor,
