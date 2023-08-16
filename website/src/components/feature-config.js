2dbebf2d433211855826dbb8345c7533e0064807
maoo <maoo@apache.org>
2023-08-16 17:46:17 +0200

    update icons

--- website/src/components/feature-config.js
+++ website/src/components/feature-config.js
@@ -3,21 +3,21 @@ import React from 'react';
 export const features = [
     {
         title: <>Fully customizable</>,
-        imageUrl: '/img/finos/finos-blue.png',
+        imageUrl: '/img/toggles.png',
         description: (
             <>Using a modular and pluggable configuration, Git Proxy allows to define manual and automatic approval workflows.</>
         )
     },
     {
         title: <>Store and forward</>,
-        imageUrl: '/img/finos/finos-blue.png',
+        imageUrl: '/img/fast-forward.png',
         description: (
             <>Git Proxy can store developers git operations while waiting for approval, then forward it without the need of developer's intervention.</>
         )
     },
     {
         title: <>Share configurations</>,
-        imageUrl: '/img/finos/finos-blue.png',
+        imageUrl: '/img/finos/share.png',
         description: (
             <>Share and reuse Git Proxy configurations across firms and individuals, by simply pointing to the extension plugins</>
         )
