b681733b4ac2dba45ffcf65b2ec6cf4441f76999
maoo <maoo@apache.org>
2023-08-22 14:14:51 +0200

    fix: removed files that are gitignored

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/m/w/git-proxy/website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/m/w/git-proxy/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/m/w/git-proxy/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/m/w/git-proxy/website/src/css/custom.css'),
-];
