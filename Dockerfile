be9b730812f6a581161faa09a64b5ca36e1950ba
Thomas Cooper <thomascooperca@gmail.com>
2023-08-27 12:40:35 -0400

    feat: experimental plugin manager
    * on proxy startup, read plugin names via environment variable
      `GITPROXY_LOAD_PLUGINS` as a comma-separated list
    * on startup, initialize PluginManager which sets a list of plugins
      and loads them via `load-plugin`. Note, this is a no-op atm since
      the actual proxy processors are not calling into plugin functions yet
    * add lerna to support multiple npm packages in a single workspace.
      This is necessary to start breaking apart the monolithic git-proxy
      package code into separate packages.
    * add HttpPlugin class which takes in a request & response function.
      This class should implement those methods. Note, this is better suited
      to TypeScript which has built-in interfaces.
    
    This doesn't "do" anything yet.
    
    feat: plugin system
    
    re-ordered hardcoded values
    
    work in progress

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18
+FROM node:18 AS build
 
 WORKDIR /usr/src/app
 
@@ -6,6 +6,11 @@ COPY package*.json ./
 
 RUN npm ci --omit=dev --ignore-scripts
 
+FROM node:18-slim
+
+WORKDIR /usr/src/app
+
+COPY --from=build /usr/src/app/node_modules/ node_modules/
 COPY src ./src/
 COPY resources ./resources/
 COPY index.js docker-entrypoint.sh ./
--- src/proxy/chain.js
+++ src/proxy/chain.js
@@ -1,4 +1,5 @@
 const proc = require('./processors');
+const plugin = require('../plugin');
 
 const pushActionChain = [
   proc.push.parsePush,
@@ -42,7 +43,25 @@ const chain = async (req) => {
 
 const getChain = (action) => {
   if (action.type === 'pull') return [];
-  if (action.type === 'push') return pushActionChain;
+  if (action.type === 'push') {
+    console.log('Checking for plugin actions');
+    // plugin.pluginManager is a Promise. How to get pluginManager.plugins if
+    // its lazily loaded and initalized async???
+    const pluginActions = plugin.pluginManager
+      .then((pm) => (pm.loaded ? pm.plugins : []))
+      .catch((err) => {
+        console.error(err);
+        return [];
+      });
+    // insert loaded plugins as actions
+    // this probably isn't the place to insert these functions
+    if (pluginActions.length > 0) {
+      console.log(`Found ${pluginActions.length}, upserting into proxy chain`);
+      pushActionChain = pushActionChain.splice(1, 0, ...pluginActions);
+      console.log(pushActionChain);
+    }
+    return pushActionChain;
+  }
 };
 
 exports.exec = chain;
