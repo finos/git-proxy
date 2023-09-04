cf5641435d0d183655e597d07730a584712e1307
Thomas Cooper <thomascooperca@gmail.com>
2023-09-04 13:01:19 -0400

    feat: actionplugin exec function + node 16
    - upgrade nodejs workflow to 16.x. New dependencies won't build on <14
    - pass in the ActionPlugin.exec function as part of the proxy chain
    - update JSDoc on ActionPlugin
    - add test for creating the PluginLoader

--- packages/git-proxy-notify-hello/index.js
+++ packages/git-proxy-notify-hello/index.js
@@ -1,11 +1,9 @@
 const Step = require('@finos/git-proxy/src/proxy/actions').Step;
 const plugin = require('@finos/git-proxy/src/plugin');
 
-const helloPlugin = new plugin.GenericPlugin((req, action) => {
-  const step = new Step('helloPlugin');
-  console.log('This is a message from the helloPlugin!');
-  console.log(`req: ${req}`);
-  console.log(`action: ${action}`);
+const helloPlugin = new plugin.ActionPlugin(async (req, action) => {
+  const step = new Step('HelloPlugin');
+  console.log('This is a message from the HelloPlugin!');
   action.addStep(step);
   return action;
 });
--- src/plugin.js
+++ src/plugin.js
@@ -1,5 +1,3 @@
-/* eslint-disable require-jsdoc */
-/* eslint-disable guard-for-in */
 const path = require('path');
 const lpModule = import('load-plugin');
 ('use strict');
@@ -7,19 +5,18 @@ const lpModule = import('load-plugin');
 /**
  * Finds, registers and loads plugins used by git-proxy
  */
-class PluginManager {
+class PluginLoader {
   /**
-   * Initialize PluginManager
+   * Initialize PluginLoader with candidates modules (node_modules or relative
+   * file paths).
    * @param {Array.<string>} names List of Node module/package names to load.
-   * @param {Array.<string>} paths List of directory paths to load modules from.
+   * @param {Array.<string>} paths List of file paths to load modules from.
    */
   constructor(names, paths) {
-    console.log(`Initializing plugin manager...`);
     this.names = names;
     this.paths = paths;
     /**
-     * @type {Array.<ProxyPlugin>} List of ProxyPlugin objects set via
-     *                             loadPlugins()
+     * @type {Array.<ProxyPlugin>} List of ProxyPlugin objects loaded.
      * @public
      */
     this.plugins = [];
@@ -29,7 +26,7 @@ class PluginManager {
    * Load configured plugins as modules and set each concrete ProxyPlugin
    * to this.plugins for use in proxying.
    */
-  loadPlugins() {
+  load() {
     const modulePromises = [];
     for (const path of this.paths) {
       modulePromises.push(this._loadFilePlugin(path));
@@ -53,6 +50,12 @@ class PluginManager {
     });
   }
 
+  /**
+   * Load a plugin module from a relative file path to the
+   * current working directory.
+   * @param {string} filepath
+   * @return {Module}
+   */
   async _loadFilePlugin(filepath) {
     const lp = await lpModule;
     const resolvedModuleFile = await lp.resolvePlugin(
@@ -61,6 +64,12 @@ class PluginManager {
     return await lp.loadPlugin(resolvedModuleFile);
   }
 
+  /**
+   * Load a plugin module from the specified Node module. Only
+   * modules with the prefix "@finos" are supported.
+   * @param {string} packageName
+   * @return {Module}
+   */
   async _loadPackagePlugin(packageName) {
     const lp = await lpModule;
     const resolvedPackageFile = await lp.resolvePlugin(packageName, {
@@ -69,12 +78,16 @@ class PluginManager {
     return await lp.loadPlugin(resolvedPackageFile);
   }
 
+  /**
+   * Set a list of ProxyPlugin objects to this.plugins
+   * from the keys exported by the passed in module.
+   * @param {Module} pluginModule
+   * @return {ProxyPlugin}
+   */
   async _castToPluginObjects(pluginModule) {
     const plugins = [];
     // iterate over the module.exports keys
     for (const key of Object.keys(pluginModule)) {
-      // only extract instances of the GenericPlugin base class
-      // add it to the list of loaded plugins
       if (
         Object.prototype.hasOwnProperty.call(pluginModule, key) &&
         pluginModule[key] instanceof ProxyPlugin
@@ -88,60 +101,46 @@ class PluginManager {
 
 /**
  * Parent class for all Git Proxy plugins. New plugin types must inherit from
- * ProxyPlugin to be loaded by PluginManager.
+ * ProxyPlugin to be loaded by PluginLoader.
  */
 class ProxyPlugin {}
 
 /**
  * A plugin which executes a function when receiving a proxy request.
  */
-class GenericPlugin extends ProxyPlugin {
+class ActionPlugin extends ProxyPlugin {
   /**
-   * Setup a new instance of GenericPlugin with a custom exec function.
-   * @param {function} execFunc A function that executes when a push is proxied.
+   * Custom function executed as part of the action chain. The function
+   * must take in two parameters, an {@link https://expressjs.com/en/4x/api.html#req Express Request}
+   * and the current Action executed in the chain.
+   * @param {Promise<Action>} exec A Promise that returns an Action &
+   *                               executes when a push is proxied.
    */
-  constructor(execFunc) {
+  constructor(exec) {
     super();
-    this.pluginType = 'generic';
-    this.execFunc = execFunc;
-  }
-
-  /**
-   *
-   * @param {*} request
-   * @param {*} action
-   */
-  async execute(request, action) {
-    await this.execFunc(request, action);
+    this.exec = exec;
   }
 }
 
-const setupPluginManager = () => {
+const createLoader = async () => {
   // Auto-register plugins that are part of git-proxy core
-  let packagePlugins = [];
-  let localPlugins = ['./packages/git-proxy-notify-hello/index.js'];
-  // How should additional plugins be registered? Environment variable?
-  if (process.env.GITPROXY_LOAD_PLUGINS) {
-    const plugins = process.env.GITPROXY_LOAD_PLUGINS.split(',');
-    plugins
-      .filter((p) => {
-        p.length !== 0;
-      })
-      .forEach((p) => {
-        // need to differentiate between plugins loaded via file path and
-        // ones loaded using Node's builtin name resolution (node_modules)
-        if (p.charAt(0) !== '@' && p.includes('/')) {
-          localPlugins = localPlugins.concat(p);
-        } else {
-          packagePlugins = packagePlugins.concat(p);
-        }
-      });
+  let names = [];
+  let files = [];
+  if (process.env.GITPROXY_PLUGIN_PACKAGES !== undefined) {
+    names = process.env.GITPROXY_PLUGIN_PACKAGES.split(',');
   }
-  const pluginMgr = new PluginManager(packagePlugins, localPlugins);
-  pluginMgr.loadPlugins();
-  return pluginMgr;
+  if (process.env.GITPROXY_PLUGIN_FILES !== undefined) {
+    files = process.env.GITPROXY_PLUGIN_FILES.split(',');
+  }
+  const loader = new PluginLoader(names, files);
+  if (names.length + files.length > 0) {
+    loader.load();
+  }
+  return loader;
 };
 
-module.exports.pluginManager = setupPluginManager();
+module.exports.defaultLoader = createLoader();
 module.exports.ProxyPlugin = ProxyPlugin;
-module.exports.GenericPlugin = GenericPlugin;
+module.exports.ActionPlugin = ActionPlugin;
+// exported for testing only
+module.exports.createLoader = createLoader;
