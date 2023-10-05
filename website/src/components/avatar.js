4cfd5372038fd3fc39cd639659dbac5200eeed14
Jamie Slome <jamie.slome@citi.com>
2023-10-05 10:25:09 +0100

    chore: create avatar component including name, description and github username

---
+++ website/src/components/avatar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Avatar component
+ * @param {*} props
+ * @return {JSX.Element}
+ */
+export default function Avatar({ name, description, username }) {
+  // add prop validation
+  Avatar.propTypes = {
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+  };
+
+  const profileUrl = `https://github.com/${username}`;
+  const imageUrl = `${profileUrl}.png`;
+
+  return (
+    <div className="avatar avatar--vertical">
+      <img
+        className="avatar__photo avatar__photo--xl margin-bottom--md"
+        src={imageUrl}
+      />
+      <div className="avatar__intro">
+        <div className="avatar__name">{name}</div>
+        <small className="avatar__subtitle">{description}</small>
+        <small className="avatar__subtitle text--bold">
+          <a href={profileUrl} target="__blank">
+            @{username}
+          </a>
+        </small>
+      </div>
+    </div>
+  );
+}
