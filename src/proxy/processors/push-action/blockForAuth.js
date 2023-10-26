c3f5b787fcd8f1c7a429214efe79147474859fe4
Jamie Slome <jamie.slome@citi.com>
2023-10-26 14:46:45 +0100

    fix: adjust terminal message on successful push and reflect changes in docs #321

--- src/proxy/processors/push-action/blockForAuth.js
+++ src/proxy/processors/push-action/blockForAuth.js
@@ -4,17 +4,13 @@ const exec = async (req, action) => {
   const step = new Step('authBlock');
 
   const message =
-    '\n\n' +
-    `Git Proxy has received your push 🎉\n` +
-    '----------------------------------------------------------\n' +
-    `   Commit from | ${action.commitFrom}\n` +
-    `    Commit to  | ${action.commitTo}\n` +
-    `       URL     | http://localhost:8080/requests/${action.id}` +
-    '\n\n';
+    '\n\n\n' +
+    `Git Proxy has received your push:\n\n` +
+    `http://localhost:8080/requests/${action.id}` +
+    '\n\n\n';
   step.setAsyncBlock(message);
   action.addStep(step);
   return action;
 };
 
-exec.displayName = 'blockForAuth.exec';
 exports.exec = exec;
