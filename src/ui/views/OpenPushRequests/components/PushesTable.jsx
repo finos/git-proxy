273d491398840fcc56822333dc4546b77e7747fc
JamieSlome <jamie.slome@citi.com>
2023-11-07 16:32:24 +0000

    chore: reconcile pushes table in src/ui/views

--- src/ui/views/OpenPushRequests/components/PushesTable.jsx
+++ src/ui/views/OpenPushRequests/components/PushesTable.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-/* eslint-disable require-jsdoc */
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import moment from 'moment';
@@ -15,12 +13,13 @@ import Paper from '@material-ui/core/Paper';
 import { Navigate } from 'react-router-dom';
 import styles from '../../../assets/jss/material-dashboard-react/views/dashboardStyle';
 import { getPushes } from '../../../services/git-push';
+import { KeyboardArrowRight } from '@material-ui/icons';
 
 export default function PushesTable(props) {
   const useStyles = makeStyles(styles);
   const classes = useStyles();
   const [data, setData] = useState([]);
-  const [auth, setAuth] = useState(true);
+  const [, setAuth] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const navigate = useNavigate();
@@ -37,50 +36,102 @@ export default function PushesTable(props) {
     getPushes(setIsLoading, setData, setAuth, setIsError, query);
   }, [props]);
 
-  if (isLoading) return <div>Loading ...</div>;
+  if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Something went wrong ...</div>;
-  if (!auth) return <Navigate to={{ pathname: '/login' }} />;
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label='simple table'>
-        <TableHead>
-          <TableRow>
-            <TableCell>Actions</TableCell>
-            <TableCell align='left'>Time</TableCell>
-            <TableCell align='left'>Repo</TableCell>
-            <TableCell align='left'>Branch</TableCell>
-            <TableCell align='left'>Commit</TableCell>
-            <TableCell align='left'>Last Author</TableCell>
-            <TableCell align='left'>Last Message</TableCell>
-            <TableCell align='left'>Commits</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell component='th' scope='row'>
-                <Button variant='contained' color='primary' onClick={() => openPush(row.id)}>
-                  Open
-                </Button>
-              </TableCell>
-              <TableCell align='left'>{moment(row.timestamp).format('yyyy-MM-DD HH:mm')}</TableCell>
-              <TableCell align='left'>{row.repo}</TableCell>
-              <TableCell align='left'>{row.branch.replace('refs/heads/', '')}</TableCell>
-              <TableCell align='left'>
-                {row.commitFrom.substring(0, 5)} - {row.commitTo.substring(0, 5)}
-              </TableCell>
-              <TableCell align='left'>
-                {row.commitData[row.commitData.length - 1].author}{' '}
-              </TableCell>
-              <TableCell align='left'>
-                {row.commitData[row.commitData.length - 1].message}{' '}
-              </TableCell>
-              <TableCell align='left'>{row.commitData.length}</TableCell>
+    <div>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell align='left'>Timestamp</TableCell>
+              <TableCell align='left'>Repository</TableCell>
+              <TableCell align='left'>Branch</TableCell>
+              <TableCell align='left'>Commit SHA</TableCell>
+              <TableCell align='left'>Committer</TableCell>
+              <TableCell align='left'>Author</TableCell>
+              <TableCell align='left'>Author E-mail</TableCell>
+              <TableCell align='left'>Commit Message</TableCell>
+              <TableCell align='left'>No. of Commits</TableCell>
+              <TableCell align='right'></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {[...data].reverse().map((row) => {
+              const repoFullName = row.repo.replace('.git', '');
+              const repoBranch = row.branch.replace('refs/heads/', '');
+
+              return (
+                <TableRow key={row.id}>
+                  <TableCell align='left'>
+                    {moment
+                      .unix(row.commitData[0].commitTs || row.commitData[0].commitTimestamp)
+                      .toString()}
+                  </TableCell>
+                  <TableCell align='left'>
+                    <a href={`https://github.com/${row.repo}`} rel='noreferrer' target='_blank'>
+                      {repoFullName}
+                    </a>
+                  </TableCell>
+                  <TableCell align='left'>
+                    <a
+                      href={`https://github.com/${repoFullName}/tree/${repoBranch}`}
+                      rel='noreferrer'
+                      target='_blank'
+                    >
+                      {repoBranch}
+                    </a>
+                  </TableCell>
+                  <TableCell align='left'>
+                    <a
+                      href={`https://github.com/${repoFullName}/commit/${row.commitTo}`}
+                      rel='noreferrer'
+                      target='_blank'
+                    >
+                      {row.commitTo.substring(0, 8)}
+                    </a>
+                  </TableCell>
+                  <TableCell align='left'>
+                    <a
+                      href={`https://github.com/${row.commitData[0].committer}`}
+                      rel='noreferrer'
+                      target='_blank'
+                    >
+                      {row.commitData[0].committer}
+                    </a>
+                  </TableCell>
+                  <TableCell align='left'>
+                    <a
+                      href={`https://github.com/${row.commitData[0].author}`}
+                      rel='noreferrer'
+                      target='_blank'
+                    >
+                      {row.commitData[0].author}
+                    </a>
+                  </TableCell>
+                  <TableCell align='left'>
+                    {row.commitData[0].authorEmail ? (
+                      <a href={`mailto:${row.commitData[0].authorEmail}`}>
+                        {row.commitData[0].authorEmail}
+                      </a>
+                    ) : (
+                      'No data...'
+                    )}{' '}
+                  </TableCell>
+                  <TableCell align='left'>{row.commitData[0].message}</TableCell>
+                  <TableCell align='left'>{row.commitData.length}</TableCell>
+                  <TableCell component='th' scope='row'>
+                    <Button variant='contained' color='primary' onClick={() => openPush(row.id)}>
+                      <KeyboardArrowRight></KeyboardArrowRight>
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
