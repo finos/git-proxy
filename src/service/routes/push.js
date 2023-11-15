2fe8e7283267bcfc8b9097e6e52f640c526a895f
JamieSlome <jamie.slome@citi.com>
2023-11-15 14:08:38 +0000

    chore: reconcile service routes except auth

--- src/service/routes/push.js
+++ src/service/routes/push.js
@@ -3,46 +3,69 @@ const router = new express.Router();
 const db = require('../../db');
 
 router.get('/', async (req, res) => {
-  if (req.user) {
-    const query = {
-      type: 'push',
-    };
+  const query = {
+    type: 'push',
+  };
 
-    for (const k in req.query) {
-      if (!k) continue;
+  for (const k in req.query) {
+    if (!k) continue;
 
-      if (k === 'limit') continue;
-      if (k === 'skip') continue;
-      let v = req.query[k];
-      if (v === 'false') v = false;
-      if (v === 'true') v = true;
-      query[k] = v;
-    }
-
-    res.send(await db.getPushes(query));
-  } else {
-    res.status(401).send({
-      message: 'not logged in',
-    });
+    if (k === 'limit') continue;
+    if (k === 'skip') continue;
+    let v = req.query[k];
+    if (v === 'false') v = false;
+    if (v === 'true') v = true;
+    query[k] = v;
   }
+
+  res.send(await db.getPushes(query));
 });
 
 router.get('/:id', async (req, res) => {
-  if (req.user) {
-    const id = req.params.id;
-    res.send(await db.getPush(id));
-  } else {
-    res.status(401).send({
-      message: 'not logged in',
-    });
-  }
+  const id = req.params.id;
+  res.send(await db.getPush(id));
 });
 
 router.post('/:id/reject', async (req, res) => {
   if (req.user) {
     const id = req.params.id;
-    const result = await db.reject(id);
-    res.send(result);
+    console.log({ id });
+
+    // Get the push request
+    const push = await db.getPush(id);
+    console.log({ push });
+
+    // Get the Internal Author of the push via their Git Account name
+    const gitAccountauthor = push.user;
+    const list = await db.getUsers({ gitAccount: gitAccountauthor });
+    console.log({ list });
+
+    if (list.length === 0) {
+      res.status(401).send({
+        message: `The git account ${gitAccountauthor} could not be found`,
+      });
+      return;
+    }
+
+    if (list[0].username.toLowerCase() === req.user.username.toLowerCase()) {
+      res.status(401).send({
+        message: `Cannot reject your own changes`,
+      });
+      return;
+    }
+
+    const isAllowed = await db.canUserApproveRejectPush(id, req.user.username);
+    console.log({ isAllowed });
+
+    if (isAllowed) {
+      const result = await db.reject(id);
+      console.log(`user ${req.user.username} rejected push request for ${id}`);
+      res.send(result);
+    } else {
+      res.status(401).send({
+        message: 'User is not authorised to reject changes',
+      });
+    }
   } else {
     res.status(401).send({
       message: 'not logged in',
@@ -51,13 +74,78 @@ router.post('/:id/reject', async (req, res) => {
 });
 
 router.post('/:id/authorise', async (req, res) => {
-  if (req.user) {
+  console.log({ req });
+
+  const questions = req.body.params.attestation;
+  console.log({ questions });
+
+  const attestationComplete = questions.every((question) => !!question.checked);
+  console.log({ attestationComplete });
+
+  if (req.user && attestationComplete) {
     const id = req.params.id;
-    const result = await db.authorise(id);
-    res.send(result);
+    console.log({ id });
+
+    // Get the push request
+    const push = await db.getPush(id);
+    console.log({ push });
+
+    // Get the Internal Author of the push via their Git Account name
+    const gitAccountauthor = push.user;
+    const list = await db.getUsers({ gitAccount: gitAccountauthor });
+    console.log({ list });
+
+    if (list.length === 0) {
+      res.status(401).send({
+        message: `The git account ${gitAccountauthor} could not be found`,
+      });
+      return;
+    }
+
+    if (list[0].username.toLowerCase() === req.user.username.toLowerCase()) {
+      res.status(401).send({
+        message: `Cannot approve your own changes`,
+      });
+      return;
+    }
+
+    // If we are not the author, now check that we are allowed to authorise on this
+    // repo
+    const isAllowed = await db.canUserApproveRejectPush(id, req.user.username);
+    if (isAllowed) {
+      console.log(`user ${req.user.username} approved push request for ${id}`);
+
+      const reviewerList = await db.getUsers({ username: req.user.username });
+      console.log({ reviewerList });
+
+      const reviewerGitAccount = reviewerList[0].gitAccount;
+      console.log({ reviewerGitAccount });
+
+      if (!reviewerGitAccount) {
+        res.status(401).send({
+          message: 'You must associate a GitHub account with your user before approving...',
+        });
+        return;
+      }
+
+      const attestation = {
+        questions,
+        timestamp: new Date(),
+        reviewer: {
+          username: req.user.username,
+          gitAccount: reviewerGitAccount,
+        },
+      };
+      const result = await db.authorise(id, attestation);
+      res.send(result);
+    } else {
+      res.status(401).send({
+        message: `user ${req.user.username} not authorised to approve push's on this project`,
+      });
+    }
   } else {
     res.status(401).send({
-      message: 'not logged in',
+      message: 'You are unauthorized to perform this action...',
     });
   }
 });
@@ -65,8 +153,20 @@ router.post('/:id/authorise', async (req, res) => {
 router.post('/:id/cancel', async (req, res) => {
   if (req.user) {
     const id = req.params.id;
-    const result = await db.cancel(id);
-    res.send(result);
+
+    const isAllowed = await db.canUserCancelPush(id, req.user.username);
+
+    if (isAllowed) {
+      const result = await db.cancel(id);
+      console.log(`user ${req.user.username} canceled push request for ${id}`);
+      res.send(result);
+    } else {
+      console.log(`user ${req.user.username} not authorised to cancel push request for ${id}`);
+      res.status(401).send({
+        message:
+          'User ${req.user.username)} not authorised to cancel push requests on this project.',
+      });
+    }
   } else {
     res.status(401).send({
       message: 'not logged in',
