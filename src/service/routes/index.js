72cfc61fbc0a3b1c531542035b306c1aa78063f5
JamieSlome <jamie.slome@citi.com>
2023-11-15 14:08:38 +0000

    chore: reconcile service routes except auth

--- src/service/routes/index.js
+++ src/service/routes/index.js
@@ -5,13 +5,15 @@ const home = require('./home');
 const repo = require('./repo');
 const users = require('./users');
 const healthcheck = require('./healthcheck');
+const config = require('./config');
 const router = new express.Router();
 
-router.use('/', home);
-router.use('/auth', auth);
+router.use('/api', home);
+router.use('/api/auth', auth);
 router.use('/api/v1/healthcheck', healthcheck);
 router.use('/api/v1/push', push);
 router.use('/api/v1/repo', repo);
 router.use('/api/v1/user', users);
+router.use('/api/v1/config', config);
 
 module.exports = router;
