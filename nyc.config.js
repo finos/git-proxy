9c5688353e732d66045a766f8c3db4f528f43add
Thomas Cooper <thomas.cooper@rbc.com>
2024-02-07 11:28:27 -0500

    fix: remove nyc include filter on PRs to test codecov
    - this commit will be reverted; DO NOT MERGE

--- nyc.config.js
+++ nyc.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable max-len */
 'use strict';
 
-const { execFileSync } = require('child_process');
+// const { execFileSync } = require('child_process');
 
-let opts = {
+const opts = {
   branches: 80,
   lines: 80,
   functions: 80,
@@ -13,29 +13,29 @@ let opts = {
 // Only generate coverage report for changed files in PR
 // see: https://github.com/actions/checkout/issues/438#issuecomment-1446882066
 //      https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
-if (process.env.GITHUB_BASE_REF !== undefined) {
-  console.log('Generating coverage report for changed files...');
-  try {
-    const baseRef = execFileSync('git', [
-      'rev-parse',
-      `origin/${process.env.GITHUB_BASE_REF}`,
-    ])
-      .toString()
-      .replace('\n', '');
-    const headRef = process.env.GITHUB_SHA;
-    const stdout = execFileSync('git', [
-      'diff',
-      '--name-only',
-      `${baseRef}..${headRef}`,
-    ]).toString();
-    opts = {
-      ...opts,
-      include: stdout.split('\n'),
-    };
-  } catch (error) {
-    console.log('Error: ', error);
-  }
-}
+// if (process.env.GITHUB_BASE_REF !== undefined) {
+//   console.log('Generating coverage report for changed files...');
+//   try {
+//     const baseRef = execFileSync('git', [
+//       'rev-parse',
+//       `origin/${process.env.GITHUB_BASE_REF}`,
+//     ])
+//       .toString()
+//       .replace('\n', '');
+//     const headRef = process.env.GITHUB_SHA;
+//     const stdout = execFileSync('git', [
+//       'diff',
+//       '--name-only',
+//       `${baseRef}..${headRef}`,
+//     ]).toString();
+//     opts = {
+//       ...opts,
+//       include: stdout.split('\n'),
+//     };
+//   } catch (error) {
+//     console.log('Error: ', error);
+//   }
+// }
 
 console.log('nyc config: ', opts);
 module.exports = opts;
