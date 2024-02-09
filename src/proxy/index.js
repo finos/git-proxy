4c0cfe8cc8547513510f6d6a6b64a212657a4535
Jamie Slome <jamie.slome@citi.com>
2024-02-09 10:28:50 +0000

    chore: re-instate default repository loading

--- src/proxy/index.js
+++ src/proxy/index.js
@@ -1,6 +1,8 @@
 const proxyApp = require('express')();
 const bodyParser = require('body-parser');
 const router = require('./routes').router;
+const config = require('../config');
+const db = require('../db');
 const { GIT_PROXY_SERVER_PORT: proxyHttpPort } = require('../config/env').Vars;
 
 const options = {
@@ -14,6 +16,19 @@ proxyApp.use(bodyParser.raw(options));
 proxyApp.use('/', router);
 
 const start = async () => {
+  // Check to see if the default repos are in the repo list
+  const defaultAuthorisedRepoList = config.getAuthorisedList();
+  const allowedList = await db.getRepos();
+
+  defaultAuthorisedRepoList.forEach(async (x) => {
+    const found = allowedList.find((y) => y.project == x.project && x.name == y.name);
+    if (!found) {
+      await db.createRepo(x);
+      await db.addUserCanPush('git-proxy', 'admin');
+      await db.addUserCanAuthorise('git-proxy', 'admin');
+    }
+  });
+
   proxyApp.listen(proxyHttpPort, () => {
     console.log(`Proxy Listening on ${proxyHttpPort}`);
   });
