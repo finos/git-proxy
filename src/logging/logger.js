4ab7b535331d49d6619f16836cf31a75e53bc484
Lewis <lewis1broadhurst@gmail.com>
2024-03-09 15:35:42 +0000

    feat: change folder name to allow logger.js to be committed

---
+++ src/logging/logger.js
@@ -0,0 +1,13 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+export default logger;
