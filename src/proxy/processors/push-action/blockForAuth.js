b2535f9e30a1dccafb4be2bcddc0d46162dd71d1
Jamie Slome <jamie.slome@citi.com>
2024-03-18 13:37:42 +0000

    chore: fix shareable link in success message and add color styling and emojis

--- src/proxy/processors/push-action/blockForAuth.js
+++ src/proxy/processors/push-action/blockForAuth.js
@@ -1,10 +1,16 @@
 const Step = require('../../actions').Step;
 
+const { GIT_PROXY_UI_PORT: uiPort } = require('../../../config/env').Vars;
+
 const exec = async (req, action) => {
   const step = new Step('authBlock');
 
   const message =
-    '\n\n\n' + `Git Proxy has received your push:\n\n` + `/admin/push/${action.id}` + '\n\n\n';
+    '\n\n\n' +
+    `\x1B[32mGit Proxy has received your push ✅\x1B[0m\n\n` +
+    '🔗 Shareable Link\n\n' +
+    `\x1B[34mhttp://localhost:${uiPort}/admin/push/${action.id}\x1B[0m` +
+    '\n\n\n';
   step.setAsyncBlock(message);
 
   action.addStep(step);
