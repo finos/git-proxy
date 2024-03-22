98bdadb8556f8b76289c200aa5d587361256aabe
Jamie Slome <jamie.slome@citi.com>
2024-03-22 12:26:57 +0000

    chore: force failing test

--- test/1.test.js
+++ test/1.test.js
@@ -13,7 +13,7 @@ describe('init', async () => {
   });
 
   it('should not be logged in', async function () {
-    const res = await chai.request(app).get('/auth/profile');
+    const res = await chai.request(app).get('/abc/auth/profile');
 
     res.should.have.status(401);
   });
