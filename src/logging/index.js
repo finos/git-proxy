eecb95764f7267b7c5d4a9bd833ccc4dd0894aeb
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2024-04-08 20:41:43 +0100

    feat: ...

--- src/logging/index.js
+++ src/logging/index.js
@@ -5,6 +5,7 @@ const cleanFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
 });
 
+// test commit
 const logger = createLogger({
   format: combine(timestamp(), format.errors({ stack: true }), cleanFormat),
   transports: [
