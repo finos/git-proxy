e61c5a13b475b79e6fb4f662be52bb0344d7204a
bill <teebill15@gmail.com>
2024-05-31 15:58:21 +0000

    chore: modified ci code

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000/',
-    
+    baseUrl: process.env.CYPRESS_BASE_URL ||'http://localhost:3000',
   },
 })
