104bd3c719ada0527da4be35bdbd77adc241b5ad
Thomas Cooper <thomas.cooper@rbc.com>
2024-08-16 03:40:08 -0400

    feat: expose plugin extension points into main package, developer docs + sample
    - update PluginLoader to use load-plugin's native Node module resolution. this library already
      handles loading of files vs modules in node_modules/ so let's use that directly
    - add configuration-based plugin loading and deprecate environment variables
    - new documentation page for plugin development. loading, writing, etc.
    - add PullActionPlugin + unify config on just "plugins"
    - add explicit exports to make importing @finos/git-proxy more straight forward for plugins
    
    In addition, this commit adds both CommonJS and ES module examples of plugins and refactors them
    into a more reflective package. It also includes updates to the site documentation, specifically
    in the Development section, which now includes details about plugins and contribution guidelines.
    
    - fix issue with json-schema-for-humans producing bad output due to post-processing (script step
      no longer required)
    - docs: add section on docs on how to update the config schema + re-generate reference doc
    - fix: don't package website with git-proxy, update .npmignore on npm pack/publish

---
+++ test/fixtures/test-package/multiple-export.js
@@ -0,0 +1,13 @@
+const { PushActionPlugin, PullActionPlugin } = require('@osp0/finos-git-proxy/plugin');
+
+
+module.exports = {
+  foo: new PushActionPlugin(async (req, action) => {
+    console.log('PushActionPlugin: ', action);
+    return action;
+  }),
+  bar: new PullActionPlugin(async (req, action) => {
+    console.log('PullActionPlugin: ', action);
+    return action;
+  }),
+}
