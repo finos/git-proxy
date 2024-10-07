bea84854008282efbcd717a8ec3ee74f1a5c3d2e
Layton Whiteley <layton.whiteley@gmail.com>
2024-10-07 07:54:33 +0200

    chore: split App component from index file

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserHistory } from 'history';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-// core components
-import Admin from './ui/layouts/Admin';
-import Login from './ui/views/Login/Login';
-import './ui/assets/css/material-dashboard-react.css';
-
-const hist = createBrowserHistory();
+import { App } from './app';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-const theme = createTheme();
 
-root.render(
-  <ThemeProvider theme={theme}>
-    <Router history={hist}>
-      <Routes>
-        <Route exact path='/admin/*' element={<Admin />} />
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/' element={<Navigate from='/' to='/admin/repo' />} />
-      </Routes>
-    </Router>
-  </ThemeProvider>,
-);
+root.render(<App />);
