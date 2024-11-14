17f0edb3f5752c0c148ce9c88b3feacccbf1e3d7
Prachit <ingleprachit91@gmail.com>
2024-11-14 21:13:00 +0530

    refactor: modified the location of sample data

--- test/CheckSensitive.test.js
+++ test/CheckSensitive.test.js
@@ -26,7 +26,7 @@ describe('Sensitive Data Detection', () => {
         const step = new Step('diff');
 
         // Create diff content simulating sensitive data in CSV
-        step.setContent(createDiffContent(['test/test_data/sensitive_data.csv']));
+        step.setContent(createDiffContent(['test/test_data/sensitveDatalikecsv/sensitive_data.csv']));
         action.addStep(step)
      
         await exec(null, action);
@@ -36,7 +36,7 @@ describe('Sensitive Data Detection', () => {
     it('should detect sensitive data in XLSX file and block execution', async () => {
         const action = new Action('action_id', 'push', 'create', Date.now(), 'owner/repo');
         const step = new Step('diff');
-        step.setContent(createDiffContent(['test/test_data/sensitive_data2.xlsx']));
+        step.setContent(createDiffContent(['test/test_data/sensitveDatalikecsv/sensitive_data2.xlsx']));
         action.addStep(step);
        
         await exec(null, action);
@@ -44,10 +44,10 @@ describe('Sensitive Data Detection', () => {
     });
 
     it('should detect sensitive data in a log file and block execution', async () => {
-      
+       // need to create the sample data
         const action = new Action('action_id', 'push', 'create', Date.now(), 'owner/repo');
         const step = new Step('diff');
-        step.setContent(createDiffContent(['test/test_data/sensitive_data3.log']));
+        step.setContent(createDiffContent(['test/test_data/sensitveDatalikecsv/sensitive_data3.log']));
         action.addStep(step);
         await exec(null, action);
         sinon.assert.calledWith(logStub, sinon.match(/Your push has been blocked due to sensitive data detection/));
@@ -58,29 +58,31 @@ describe('Sensitive Data Detection', () => {
        
         const action = new Action('action_id', 'push', 'create', Date.now(), 'owner/repo');
         const step = new Step('diff');
-        step.setContent(createDiffContent(['test/test_data/sensitive_data4.json']));
+        step.setContent(createDiffContent(['test/test_data/sensitveDatalikecsv/sensitive_data4.json']));
         action.addStep(step);
         await exec(null, action);
         sinon.assert.calledWith(logStub, sinon.match(/Your push has been blocked due to sensitive data detection/));
     });
 
     it('should allow execution if no sensitive data is found', async () => {
+         // need to create sample data
        
       
         const action = new Action('action_id', 'push', 'create', Date.now(), 'owner/repo');
         const step = new Step('diff');
-        step.setContent(createDiffContent(['test_data/no_sensitive_data.txt']));
+        step.setContent(createDiffContent(['test_data/sensitveDatalikecsv/no_sensitive_data.txt']));
         action.addStep(step);
         await exec(null, action);
         sinon.assert.neverCalledWith(logStub, sinon.match(/Your push has been blocked due to sensitive data detection/));
     });
 
     it('should allow execution for an empty file', async () => {
+        // need to create sample data
        
       
         const action = new Action('action_id', 'push', 'create', Date.now(), 'owner/repo');
         const step = new Step('diff');
-        step.setContent(createDiffContent(['test_data/empty_file.txt']));
+        step.setContent(createDiffContent(['test_data/sensitveDatalikecsv/empty_file.txt']));
         action.addStep(step);
         await exec(null, action);
         sinon.assert.neverCalledWith(logStub, sinon.match(/Your push has been blocked due to sensitive data detection/));
@@ -90,7 +92,7 @@ describe('Sensitive Data Detection', () => {
         
         const action = new Action('action_id', 'push', 'create', Date.now(), 'owner/repo');
         const step = new Step('diff');
-        step.setContent(createDiffContent(['test_data/non_existent_file.txt']));
+        step.setContent(createDiffContent(['test_data/sensitveDatalikecsv/non_existent_file.txt']));
         action.addStep(step);
         try {
             await exec(null, action);
