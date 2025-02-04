256523f4a0704c5cd10cac0f0014aa5ffdf8aa61
Juan Escalada <juanescalada175@gmail.com>
2025-02-04 13:56:32 +0900

    feat(auth): add AuthProvider and PrivateRoute wrapper

---
+++ src/ui/components/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../auth/AuthProvider';
+
+const PrivateRoute = ({ component: Component, adminOnly = false }) => {
+  const { user, isLoading } = useAuth();
+  console.debug('PrivateRoute', { user, isLoading, adminOnly });
+
+  if (isLoading) {
+    console.debug('Auth is loading, waiting');
+    return <div>Loading...</div>; // TODO: Add loading spinner
+  }
+
+  if (!user) {
+    console.debug('User not logged in, redirecting to login page');
+    return <Navigate to="/login" />;
+  }
+
+  if (adminOnly && !user.isAdmin) {
+    console.debug('User is not an admin, redirecting to not authorized page');
+    return <Navigate to="/not-authorized" />;
+  }
+
+  return <Component />;
+};
+
+export default PrivateRoute;
