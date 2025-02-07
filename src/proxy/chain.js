b6953fecee2a209995f216b71d9aea3f20aa60c8
Fabio Vincenzi <fabio.vincenzi2001@gmail.com>
2025-02-07 22:38:53 +0100

    fix: change position of preReceive in the chain

--- src/proxy/chain.js
+++ src/proxy/chain.js
@@ -2,7 +2,6 @@ const proc = require('./processors');
 
 const pushActionChain = [
   proc.push.parsePush,
-  proc.push.preReceive,
   proc.push.checkRepoInAuthorisedList,
   proc.push.checkCommitMessages,
   proc.push.checkAuthorEmails,
@@ -10,6 +9,7 @@ const pushActionChain = [
   proc.push.checkIfWaitingAuth,
   proc.push.pullRemote,
   proc.push.writePack,
+  proc.push.preReceive,
   proc.push.getDiff,
   proc.push.clearBareClone,
   proc.push.scanDiff,
