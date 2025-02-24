b1abc41f01c3cb0b8c37c0b110a68a4c283ca466
Juan Escalada <juanescalada175@gmail.com>
2025-02-24 16:12:07 +0900

    refactor(ts): change db module to use TS and ESM

--- src/db/helper.ts
+++ src/db/helper.ts
@@ -1,7 +1,5 @@
-const toClass = function (obj, proto) {
+export const toClass = function (obj: any, proto: any) {
   obj = JSON.parse(JSON.stringify(obj));
   obj.__proto__ = proto;
   return obj;
 };
-
-module.exports.toClass = toClass;
