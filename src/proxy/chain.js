af74f3c59b8b46c0fcacb61106153f0c70b2025b
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2025-03-06 18:22:49 +0100

    feat: push if autoApproved=true

--- src/proxy/chain.js
+++ src/proxy/chain.js
@@ -1,4 +1,5 @@
 const proc = require('./processors');
+const db = require('../db');
 
 const pushActionChain = [
   proc.push.parsePush,
@@ -20,6 +21,22 @@ const pullActionChain = [proc.push.checkRepoInAuthorisedList];
 
 let pluginsInserted = false;
 
+const attemptAutoApproval = async (req, action) => {
+  try {
+    const attestation = {
+      timestamp: new Date(),
+      autoApproved: true,
+    };
+    await db.authorise(action.id, attestation);
+    console.log('Push automatically approved by system.');
+
+    return true;
+  } catch (error) {
+    console.error('Error during auto-approval:', error.message);
+    return false;
+  }
+};
+
 const executeChain = async (req) => {
   let action;
   try {
@@ -40,6 +57,9 @@ const executeChain = async (req) => {
     }
   } finally {
     await proc.push.audit(req, action);
+    if (action.autoApproved) {
+      attemptAutoApproval(req, action);
+    }
   }
 
   return action;
