01ac9513344cf74b40d4545f582f70ae56d0bfe5
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2025-03-25 22:42:21 +0100

    fix: fix dashboard data not displayng

--- src/ui/views/OpenPushRequests/components/PushesTable.jsx
+++ src/ui/views/OpenPushRequests/components/PushesTable.jsx
@@ -14,7 +14,7 @@ import styles from '../../../assets/jss/material-dashboard-react/views/dashboard
 import { getPushes } from '../../../services/git-push';
 import { KeyboardArrowRight } from '@material-ui/icons';
 import Search from '../../../components/Search/Search'; // Import the Search component
-import Pagination from '../../../components/Pagination/Pagination';  // Import Pagination component
+import Pagination from '../../../components/Pagination/Pagination'; // Import Pagination component
 
 export default function PushesTable(props) {
   const useStyles = makeStyles(styles);
@@ -30,79 +30,46 @@ export default function PushesTable(props) {
   const [searchTerm, setSearchTerm] = useState(''); // Define searchTerm state
   const openPush = (push) => navigate(`/admin/push/${push}`, { replace: true });
 
-    
-  
-
   useEffect(() => {
     const query = {};
-
     for (const k in props) {
-      if (!k) continue;
-      query[k] = props[k];
+      if (k) query[k] = props[k];
     }
     getPushes(setIsLoading, setData, setAuth, setIsError, query);
   }, [props]);
 
-
+  useEffect(() => {
+    setFilteredData(data);
+  }, [data]);
 
   useEffect(() => {
-    // Initialize filtered data with full data on load
-    const filtered = filterByStatus(data);
+    const lowerCaseTerm = searchTerm.toLowerCase();
+    const filtered = searchTerm
+      ? data.filter(
+          (item) =>
+            item.repo.toLowerCase().includes(lowerCaseTerm) ||
+            item.commitTo.toLowerCase().includes(lowerCaseTerm) ||
+            item.commitData[0].message.toLowerCase().includes(lowerCaseTerm),
+        )
+      : data;
     setFilteredData(filtered);
-  }, [props]);
-
-  const filterByStatus = (data) => {
-    if (props.authorised) {
-      return data.filter(item => item.status === 'approved');
-    }
-    if (props.rejected) {
-      return data.filter(item => item.status === 'rejected');
-    }
-    if (props.canceled) {
-      return data.filter(item => item.status === 'canceled');
-    }
-    if (props.blocked) {
-      return data.filter(item => item.status === 'pending');
-    }
-    return data;
-  };
-
-
-  // Apply search to the filtered data
-  useEffect(() => {
-    const filtered = filterByStatus(data); // Apply status filter first
-    if (searchTerm) {
-      const lowerCaseTerm = searchTerm.toLowerCase();
-      const searchFiltered = filtered.filter((item) =>
-        item.repo.toLowerCase().includes(lowerCaseTerm) ||
-        item.commitTo.toLowerCase().includes(lowerCaseTerm) ||
-    
-        item.commitData[0].message.toLowerCase().includes(lowerCaseTerm)
-      );
-      setFilteredData(searchFiltered);
-    } else {
-      setFilteredData(filtered); // Reset to filtered data after clearing search
-    }
-    setCurrentPage(1); // Reset pagination on search
-  }, [searchTerm, props]); // Trigger on search or tab change
+    setCurrentPage(1);
+  }, [searchTerm, data]);
 
   // Handler function for search input
-  const handleSearch = (searchTerm) => {
-    setSearchTerm(searchTerm); // Update search term state
-  };
-
+  const handleSearch = (term) => setSearchTerm(term.trim());
 
   const handlePageChange = (page) => {
-    setCurrentPage(page);  // Update current page
+    setCurrentPage(page); // Update current page
   };
 
-   // Logic for pagination (getting items for the current page)
-   const indexOfLastItem = currentPage * itemsPerPage;
-   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-   const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
- 
-   // Change page
-   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  // Logic for pagination (getting items for the current page)
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Something went wrong ...</div>;
@@ -110,22 +77,20 @@ export default function PushesTable(props) {
   return (
     <div>
       <Search onSearch={handleSearch} /> {/* Use the Search component */}
-
-      
       <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
+        <Table className={classes.table} aria-label='simple table'>
           <TableHead>
             <TableRow>
-              <TableCell align="left">Timestamp</TableCell>
-              <TableCell align="left">Repository</TableCell>
-              <TableCell align="left">Branch</TableCell>
-              <TableCell align="left">Commit SHA</TableCell>
-              <TableCell align="left">Committer</TableCell>
-              <TableCell align="left">Author</TableCell>
-              <TableCell align="left">Author E-mail</TableCell>
-              <TableCell align="left">Commit Message</TableCell>
-              <TableCell align="left">No. of Commits</TableCell>
-              <TableCell align="right"></TableCell>
+              <TableCell align='left'>Timestamp</TableCell>
+              <TableCell align='left'>Repository</TableCell>
+              <TableCell align='left'>Branch</TableCell>
+              <TableCell align='left'>Commit SHA</TableCell>
+              <TableCell align='left'>Committer</TableCell>
+              <TableCell align='left'>Author</TableCell>
+              <TableCell align='left'>Author E-mail</TableCell>
+              <TableCell align='left'>Commit Message</TableCell>
+              <TableCell align='left'>No. of Commits</TableCell>
+              <TableCell align='right'></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -135,53 +100,53 @@ export default function PushesTable(props) {
 
               return (
                 <TableRow key={row.id}>
-                  <TableCell align="left">
+                  <TableCell align='left'>
                     {moment
                       .unix(row.commitData[0].commitTs || row.commitData[0].commitTimestamp)
                       .toString()}
                   </TableCell>
-                  <TableCell align="left">
-                    <a href={`https://github.com/${row.repo}`} rel="noreferrer" target="_blank">
+                  <TableCell align='left'>
+                    <a href={`https://github.com/${row.repo}`} rel='noreferrer' target='_blank'>
                       {repoFullName}
                     </a>
                   </TableCell>
-                  <TableCell align="left">
+                  <TableCell align='left'>
                     <a
                       href={`https://github.com/${repoFullName}/tree/${repoBranch}`}
-                      rel="noreferrer"
-                      target="_blank"
+                      rel='noreferrer'
+                      target='_blank'
                     >
                       {repoBranch}
                     </a>
                   </TableCell>
-                  <TableCell align="left">
+                  <TableCell align='left'>
                     <a
                       href={`https://github.com/${repoFullName}/commit/${row.commitTo}`}
-                      rel="noreferrer"
-                      target="_blank"
+                      rel='noreferrer'
+                      target='_blank'
                     >
                       {row.commitTo.substring(0, 8)}
                     </a>
                   </TableCell>
-                  <TableCell align="left">
+                  <TableCell align='left'>
                     <a
                       href={`https://github.com/${row.commitData[0].committer}`}
-                      rel="noreferrer"
-                      target="_blank"
+                      rel='noreferrer'
+                      target='_blank'
                     >
                       {row.commitData[0].committer}
                     </a>
                   </TableCell>
-                  <TableCell align="left">
+                  <TableCell align='left'>
                     <a
                       href={`https://github.com/${row.commitData[0].author}`}
-                      rel="noreferrer"
-                      target="_blank"
+                      rel='noreferrer'
+                      target='_blank'
                     >
                       {row.commitData[0].author}
                     </a>
                   </TableCell>
-                  <TableCell align="left">
+                  <TableCell align='left'>
                     {row.commitData[0].authorEmail ? (
                       <a href={`mailto:${row.commitData[0].authorEmail}`}>
                         {row.commitData[0].authorEmail}
@@ -190,10 +155,10 @@ export default function PushesTable(props) {
                       'No data...'
                     )}
                   </TableCell>
-                  <TableCell align="left">{row.commitData[0].message}</TableCell>
-                  <TableCell align="left">{row.commitData.length}</TableCell>
-                  <TableCell component="th" scope="row">
-                    <Button variant="contained" color="primary" onClick={() => openPush(row.id)}>
+                  <TableCell align='left'>{row.commitData[0].message}</TableCell>
+                  <TableCell align='left'>{row.commitData.length}</TableCell>
+                  <TableCell component='th' scope='row'>
+                    <Button variant='contained' color='primary' onClick={() => openPush(row.id)}>
                       <KeyboardArrowRight />
                     </Button>
                   </TableCell>
@@ -203,18 +168,14 @@ export default function PushesTable(props) {
           </TableBody>
         </Table>
       </TableContainer>
-  {/* Pagination Component */}
-  <Pagination
+      {/* Pagination Component */}
+      <Pagination
         itemsPerPage={itemsPerPage}
         totalItems={filteredData.length}
         paginate={paginate}
         currentPage={currentPage}
-        onPageChange={handlePageChange} 
+        onPageChange={handlePageChange}
       />
     </div>
   );
 }
-
-
-
-
