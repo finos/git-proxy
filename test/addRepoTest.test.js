1847c0ab83832aa108afc288359eb66f34057a5f
Kris West <kristopher.west@natwest.com>
2025-04-01 14:22:18 +0100

    test: use unique emails for users in tests and remove afterwards

--- test/addRepoTest.test.js
+++ test/addRepoTest.test.js
@@ -187,10 +187,8 @@ describe('add new repo', async () => {
 
   after(async function () {
     await service.httpServer.close();
-
-    // don't clean up data as cypress tests rely on it being present
-    // await db.deleteRepo('test-repo');
-    // await db.deleteUser('u1');
-    // await db.deleteUser('u2');
+    await db.deleteRepo('test-repo');
+    await db.deleteUser('u1');
+    await db.deleteUser('u2');
   });
 });
