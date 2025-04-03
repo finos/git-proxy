6c4e408f4da6d2355010d66b80e88fab02313226
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2025-04-03 15:40:05 +0200

    refactor(tsx): convert utils file

--- src/ui/utils.tsx
+++ src/ui/utils.tsx
@@ -1,17 +1,17 @@
 /**
  * Retrieve a decoded cookie value from `document.cookie` with given `name`.
- * @param {string} name
- * @return {string}
+ * @param {string} name - The name of the cookie to retrieve
+ * @return {string | null} - The cookie value or null if not found
  */
-export const getCookie = (name) => {
+export const getCookie = (name: string): string | null => {
   if (!document.cookie) return null;
-
+  
   const cookies = document.cookie
     .split(';')
     .map((c) => c.trim())
     .filter((c) => c.startsWith(name + '='));
-
+    
   if (!cookies.length) return null;
-
+  
   return decodeURIComponent(cookies[0].split('=')[1]);
 };
