7d10949a2208531426dc49d8112f6f7202358046
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2025-04-03 15:35:40 +0200

    refactor(tsx): convert index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createBrowserHistory } from 'history';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { AuthProvider } from './ui/auth/AuthProvider';
 
@@ -11,16 +10,14 @@ import './ui/assets/css/material-dashboard-react.css';
 import NotAuthorized from './ui/views/Extras/NotAuthorized';
 import NotFound from './ui/views/Extras/NotFound';
 
-const hist = createBrowserHistory();
-
 ReactDOM.render(
   <AuthProvider>
-    <Router history={hist}>
+    <Router>
       <Routes>
-        <Route exact path='/dashboard/*' element={<Dashboard />} />
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/not-authorized' element={<NotAuthorized />} />
-        <Route exact path='/' element={<Navigate from='/' to='/dashboard/repo' />} />
+        <Route path='/dashboard/*' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/not-authorized' element={<NotAuthorized />} />
+        <Route path='/' element={<Navigate to='/dashboard/repo' />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
