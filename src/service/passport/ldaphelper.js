644e571f6dd5f5c01749cd61860f143aaddc4159
kriswest <kristopher.west@natwest.com>
2025-04-10 15:58:20 +0100

    feat: support direct querying of AD group membership via LDAP

--- src/service/passport/ldaphelper.js
+++ src/service/passport/ldaphelper.js
@@ -1,17 +1,47 @@
-const axios = require('axios');
+const config = require('../../config').getAuthentication();
 const thirdpartyApiConfig = require('../../config').getAPIs();
-const client = axios.create({
-  responseType: 'json',
-  headers: {
-    'content-type': 'application/json',
-  },
-});
+const axios = require('axios');
 
-const isUserInAdGroup = (id, domain, name) => {
+const isUserInAdGroup = (req, profile, ad, domain, name) => {
+  // determine, via config, if we're using HTTP or AD directly
+  if (thirdpartyApiConfig?.ls?.userInADGroup) {
+    return isUserInAdGroupViaHttp(profile.username, domain, name);
+  } else if (config.adConfig) {
+    return isUserInAdGroupViaAD(req, profile, ad, domain, name);
+  } else {
+    console.error('Unable to check user groups as config is incomplete or unreadable');
+  }
+};
+
+const isUserInAdGroupViaAD = (req, profile, ad, domain, name) => {
+  // TODO: Added for debugging , needs to be removed
+  console.log(`checking if id: ${profile.username}, on domain: ${domain}, is a member of group name: ${name}`);
+
+  return new Promise((resolve, reject) => {
+    ad.isUserMemberOf(profile.username, name, function (err, isMember) {
+      if (err) {
+        const msg = 'ERROR isUserMemberOf: ' + JSON.stringify(err);
+        reject(msg);
+      } else {
+        console.log(profile.username + ' isMemberOf ' + name + ': ' + isMember);
+        resolve(isMember);
+      }
+    });
+  });
+};
+
+const isUserInAdGroupViaHttp = (id, domain, name) => {
   const url = String(thirdpartyApiConfig.ls.userInADGroup)
     .replace('<domain>', domain)
     .replace('<name>', name)
     .replace('<id>', id);
+
+  const client = axios.create({
+    responseType: 'json',
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
 
   console.log(`checking if user is in group ${url}`);
   return client
