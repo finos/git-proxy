RBC/finos-git-proxy

fix: add tls object to bypass certs

src/config/index.ts
@@ -2,8 +2,13 @@ import { existsSync, readFileSync } from 'fs';
 
 import defaultSettings from '../../proxy.config.json';
 import { configFile } from './file';
-import { Authentication, AuthorisedRepo, Database, TempPasswordConfig, UserSettings } from './types';
-
+import {
+  Authentication,
+  AuthorisedRepo,
+  Database,
+  TempPasswordConfig,
+  UserSettings,
+} from './types';
 
 let _userSettings: UserSettings | null = null;
 if (existsSync(configFile)) {
@@ -26,8 +31,9 @@ let _contactEmail: string = defaultSettings.contactEmail;
 let _csrfProtection: boolean = defaultSettings.csrfProtection;
 let _domains: Record<string, unknown> = defaultSettings.domains;
 // These are not always present in the default config file, so casting is required
-let _sslKeyPath: string = (defaultSettings as any).sslKeyPemPath;
-let _sslCertPath: string = (defaultSettings as any).sslCertPemPath;
+let _tlsEnabled = defaultSettings.tls.enabled;
+let _tlsKeyPemPath = defaultSettings.tls.key;
+let _tlsCertPemPath = defaultSettings.tls.cert;
 
 // Get configured proxy URL
 export const getProxyUrl = () => {
@@ -170,26 +176,42 @@ export const getPlugins = () => {
     _plugins = _userSettings.plugins;
   }
   return _plugins;
-}
-
-export const getSSLKeyPath = () => {
-  if (_userSettings && _userSettings.sslKeyPemPath) {
-    _sslKeyPath = _userSettings.sslKeyPemPath;
-  }
-  if (!_sslKeyPath) {
-    return '../../certs/key.pem';
-  }
-  return _sslKeyPath;
 };
 
-export const getSSLCertPath = () => {
-  if (_userSettings && _userSettings.sslCertPemPath) {
-    _sslCertPath = _userSettings.sslCertPemPath;
+export const getTLSKeyPemPath = () => {
+  if (_userSettings && _userSettings.tlsKeyPemPath) {
+    console.log(
+      'Warning: tlsKeyPemPath setting is replaced with tls.key setting in proxy.config.json & will be deprecated in a future release',
+    );
+    _tlsKeyPemPath = _userSettings.tlsKeyPemPath;
   }
-  if (!_sslCertPath) {
+  if (!_tlsKeyPemPath) {
+    return '../../certs/key.pem';
+  }
+  return _tlsKeyPemPath;
+};
+
+export const getTLSCertPemPath = () => {
+  if (_userSettings && _userSettings.tlsCertPemPath) {
+    console.log(
+      'Warning: tlsCertPemPath setting is replaced with tls.cert setting in proxy.config.json & will be deprecated in a future release',
+    );
+    _tlsCertPemPath = _userSettings.tlsCertPemPath;
+  }
+  if (!_tlsCertPemPath) {
     return '../../certs/cert.pem';
   }
-  return _sslCertPath;
+  return _tlsCertPemPath;
+};
+
+export const getTLSEnabled = () => {
+  if (_userSettings && _userSettings.tlsEnabled) {
+    _tlsEnabled = _userSettings.tlsEnabled;
+  }
+  if (!_tlsEnabled) {
+    return false;
+  }
+  return _tlsEnabled;
 };
 
 export const getDomains = () => {

test/testConfig.test.js
@@ -16,8 +16,8 @@ describe('default configuration', function () {
     expect(config.getDatabase()).to.be.eql(defaultSettings.sink[0]);
     expect(config.getTempPasswordConfig()).to.be.eql(defaultSettings.tempPassword);
     expect(config.getAuthorisedList()).to.be.eql(defaultSettings.authorisedList);
-    expect(config.getSSLKeyPath()).to.be.eql("../../certs/key.pem");
-    expect(config.getSSLCertPath()).to.be.eql("../../certs/cert.pem");
+    expect(config.getSSLKeyPath()).to.be.eql(defaultSettings.tls.key);
+    expect(config.getSSLCertPath()).to.be.eql(defaultSettings.tls.cert);
   });
   after(function () {
     delete require.cache[require.resolve('../src/config')];
@@ -94,15 +94,17 @@ describe('user configuration', function () {
 
   it('should override default settings for SSL certificate', function () {
     const user = {
-      sslKeyPemPath: "my-key.pem",
-      sslCertPemPath: "my-cert.pem"
+      tls: {
+        key: 'my-key.pem',
+        cert: 'my-cert.pem',
+      },
     };
     fs.writeFileSync(tempUserFile, JSON.stringify(user));
 
     const config = require('../src/config');
 
-    expect(config.getSSLKeyPath()).to.be.eql(user.sslKeyPemPath);
-    expect(config.getSSLCertPath()).to.be.eql(user.sslCertPemPath);
+    expect(config.getSSLKeyPath()).to.be.eql(user.tls.key);
+    expect(config.getSSLCertPath()).to.be.eql(user.tls.cert);
   });
 
   afterEach(function () {
@@ -116,21 +118,14 @@ describe('validate config files', function () {
   const config = require('../src/config/file');
 
   it('all valid config files should pass validation', function () {
-    const validConfigFiles = [
-      'proxy.config.valid-1.json',
-      'proxy.config.valid-2.json',
-    ];
+    const validConfigFiles = ['proxy.config.valid-1.json', 'proxy.config.valid-2.json'];
     for (const testConfigFile of validConfigFiles) {
-      expect(config.validate(path.join(__dirname, fixtures, testConfigFile))).to
-        .be.true;
+      expect(config.validate(path.join(__dirname, fixtures, testConfigFile))).to.be.true;
     }
   });
 
   it('all invalid config files should fail validation', function () {
-    const invalidConfigFiles = [
-      'proxy.config.invalid-1.json',
-      'proxy.config.invalid-2.json',
-    ];
+    const invalidConfigFiles = ['proxy.config.invalid-1.json', 'proxy.config.invalid-2.json'];
     for (const testConfigFile of invalidConfigFiles) {
       const test = function () {
         config.validate(path.join(__dirname, fixtures, testConfigFile));

finos/git-proxy

src/config/index.ts
@@ -2,8 +2,13 @@ import { existsSync, readFileSync } from 'fs';
 
 import defaultSettings from '../../proxy.config.json';
 import { configFile } from './file';
-import { Authentication, AuthorisedRepo, Database, TempPasswordConfig, UserSettings } from './types';
-
+import {
+  Authentication,
+  AuthorisedRepo,
+  Database,
+  TempPasswordConfig,
+  UserSettings,
+} from './types';
 
 let _userSettings: UserSettings | null = null;
 if (existsSync(configFile)) {
@@ -26,8 +31,9 @@ let _contactEmail: string = defaultSettings.contactEmail;
 let _csrfProtection: boolean = defaultSettings.csrfProtection;
 let _domains: Record<string, unknown> = defaultSettings.domains;
 // These are not always present in the default config file, so casting is required
-let _sslKeyPath: string = (defaultSettings as any).sslKeyPemPath;
-let _sslCertPath: string = (defaultSettings as any).sslCertPemPath;
+let _tlsEnabled = defaultSettings.tls.enabled;
+let _tlsKeyPemPath = defaultSettings.tls.key;
+let _tlsCertPemPath = defaultSettings.tls.cert;
 
 // Get configured proxy URL
 export const getProxyUrl = () => {
@@ -170,26 +176,39 @@ export const getPlugins = () => {
     _plugins = _userSettings.plugins;
   }
   return _plugins;
-}
-
-export const getSSLKeyPath = () => {
-  if (_userSettings && _userSettings.sslKeyPemPath) {
-    _sslKeyPath = _userSettings.sslKeyPemPath;
-  }
-  if (!_sslKeyPath) {
-    return '../../certs/key.pem';
-  }
-  return _sslKeyPath;
 };
 
-export const getSSLCertPath = () => {
+export const getTLSKeyPemPath = () => {
+  if (_userSettings && _userSettings.sslKeyPemPath) {
+    console.log(
+      'Warning: sslKeyPemPath setting is replaced with tls.key setting in proxy.config.json & will be deprecated in a future release',
+    );
+    _tlsKeyPemPath = _userSettings.sslKeyPemPath;
+  }
+  if (_userSettings?.tls && _userSettings?.tls?.key) {
+    _tlsKeyPemPath = _userSettings.tls.key;
+  }
+  return _tlsKeyPemPath;
+};
+
+export const getTLSCertPemPath = () => {
   if (_userSettings && _userSettings.sslCertPemPath) {
-    _sslCertPath = _userSettings.sslCertPemPath;
+    console.log(
+      'Warning: sslCertPemPath setting is replaced with tls.cert setting in proxy.config.json & will be deprecated in a future release',
+    );
+    _tlsCertPemPath = _userSettings.sslCertPemPath;
   }
-  if (!_sslCertPath) {
-    return '../../certs/cert.pem';
+  if (_userSettings?.tls && _userSettings?.tls?.cert) {
+    _tlsCertPemPath = _userSettings.tls.cert;
   }
-  return _sslCertPath;
+  return _tlsCertPemPath;
+};
+
+export const getTLSEnabled = () => {
+  if (_userSettings && _userSettings.tls && _userSettings.tls.enabled) {
+    _tlsEnabled = _userSettings.tls.enabled;
+  }
+  return _tlsEnabled;
 };
 
 export const getDomains = () => {

test/testConfig.test.js
@@ -16,8 +16,8 @@ describe('default configuration', function () {
     expect(config.getDatabase()).to.be.eql(defaultSettings.sink[0]);
     expect(config.getTempPasswordConfig()).to.be.eql(defaultSettings.tempPassword);
     expect(config.getAuthorisedList()).to.be.eql(defaultSettings.authorisedList);
-    expect(config.getSSLKeyPath()).to.be.eql("../../certs/key.pem");
-    expect(config.getSSLCertPath()).to.be.eql("../../certs/cert.pem");
+    expect(config.getTLSKeyPemPath()).to.be.eql(defaultSettings.tls.key);
+    expect(config.getTLSCertPemPath()).to.be.eql(defaultSettings.tls.cert);
   });
   after(function () {
     delete require.cache[require.resolve('../src/config')];
@@ -94,15 +94,17 @@ describe('user configuration', function () {
 
   it('should override default settings for SSL certificate', function () {
     const user = {
-      sslKeyPemPath: "my-key.pem",
-      sslCertPemPath: "my-cert.pem"
+      tls: {
+        key: 'my-key.pem',
+        cert: 'my-cert.pem',
+      },
     };
     fs.writeFileSync(tempUserFile, JSON.stringify(user));
 
     const config = require('../src/config');
 
-    expect(config.getSSLKeyPath()).to.be.eql(user.sslKeyPemPath);
-    expect(config.getSSLCertPath()).to.be.eql(user.sslCertPemPath);
+    expect(config.getTLSKeyPemPath()).to.be.eql(user.tls.key);
+    expect(config.getTLSCertPemPath()).to.be.eql(user.tls.cert);
   });
 
   afterEach(function () {
@@ -116,21 +118,14 @@ describe('validate config files', function () {
   const config = require('../src/config/file');
 
   it('all valid config files should pass validation', function () {
-    const validConfigFiles = [
-      'proxy.config.valid-1.json',
-      'proxy.config.valid-2.json',
-    ];
+    const validConfigFiles = ['proxy.config.valid-1.json', 'proxy.config.valid-2.json'];
     for (const testConfigFile of validConfigFiles) {
-      expect(config.validate(path.join(__dirname, fixtures, testConfigFile))).to
-        .be.true;
+      expect(config.validate(path.join(__dirname, fixtures, testConfigFile))).to.be.true;
     }
   });
 
   it('all invalid config files should fail validation', function () {
-    const invalidConfigFiles = [
-      'proxy.config.invalid-1.json',
-      'proxy.config.invalid-2.json',
-    ];
+    const invalidConfigFiles = ['proxy.config.invalid-1.json', 'proxy.config.invalid-2.json'];
     for (const testConfigFile of invalidConfigFiles) {
       const test = function () {
         config.validate(path.join(__dirname, fixtures, testConfigFile));