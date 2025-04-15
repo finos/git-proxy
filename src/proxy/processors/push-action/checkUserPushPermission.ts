96cab2d3b1445ceefcbc9874e3a01c169d652ebd
Kris West <kristopher.west@natwest.com>
2025-04-15 15:24:57 +0100

    test: coverage for checkUserPushPermission

--- src/proxy/processors/push-action/checkUserPushPermission.ts
+++ src/proxy/processors/push-action/checkUserPushPermission.ts
@@ -11,17 +11,17 @@ const exec = async (req: any, action: Action): Promise<Action> => {
   // n.b. action.user will be set to whatever the user had set in their user.name config in their git client.
   // it is not necessarily the GitHub username. GitHub looks users by email address as should we
   const userEmail = action.userEmail;
-  let username = "unknown";
+  let username = 'unknown';
 
   // Find the user associated with this email address
   const list = await getUsers({ email: action.userEmail });
 
   if (list.length > 1) {
     console.warn(`Multiple users found with email address ${userEmail}, using the first only`);
-  } else if (list.length == 0){ 
+  } else if (list.length == 0) {
     console.error(`No user with email address ${userEmail} found`);
   } else {
-    username = list[0].username
+    username = list[0].username;
     isUserAllowed = await isUserPushAllowed(repoName, username);
   }
 
@@ -30,14 +30,16 @@ const exec = async (req: any, action: Action): Promise<Action> => {
   if (!isUserAllowed) {
     console.log('User not allowed to Push');
     step.error = true;
-    step.log(`User ${username} <${userEmail}> is not allowed to push on repo ${action.repo}, ending`);
+    step.log(
+      `User ${username} <${userEmail}> is not allowed to push on repo ${action.repo}, ending`,
+    );
 
     console.log('setting error');
 
     step.setError(
-      `Rejecting push as user ${action.user} ` +
-      `is not allowed to push on repo ` +
-      `${action.repo}`,
+      `Your push has been blocked (${action.userEmail} ` +
+        `is not allowed to push on repo ` +
+        `${action.repo})`,
     );
     action.addStep(step);
     return action;
