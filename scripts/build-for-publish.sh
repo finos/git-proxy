53bd6d7a55b022c5d138a81c1f475e9721e4b3a6
Jack Kelly <jack@control-plane.io>
2025-05-14 15:44:25 +0100

    ci: add guardrails and add to publish

--- scripts/build-for-publish.sh
+++ scripts/build-for-publish.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
 
 # This script allows for emitting js and definitions from the typescript into
 # the same import locations as the original files.
 # When we adjust how we import the library we can move to a "dist" folder and
 # explicit "exports".
+
+if [ "${IS_PUBLISHING:-}" != "YES" ]; then
+  echo "This script is intended to prepare the directory for publishing"
+  echo "and replaces files. If you only want to build the UI run \`npm run build-ui\`."
+  echo "Otherwise set IS_PUBLISHING to \"YES\""
+  exit 1
+fi
+
+set -x
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 cd "$REPO_ROOT"
