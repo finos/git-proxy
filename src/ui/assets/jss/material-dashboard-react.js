0a882a4cb0db0a0fc59e6d7cc94493de1dba4e08
Sam Holmes <sam-holmes2@users.noreply.github.com>
2025-05-21 12:07:56 +0100

    updated license header to shorter version and applied to relevant code files (.js, .ts, .tsx, .jsx and .sh files) only. Also updated LICENSE and NOTICE files to contain list of contributors.

--- src/ui/assets/jss/material-dashboard-react.js
+++ src/ui/assets/jss/material-dashboard-react.js
@@ -1,17 +1,20 @@
-/* !
-
- =========================================================
- * Material Dashboard React - v1.9.0 based on Material Dashboard - v1.2.0
- =========================================================
-
- * Product Page: http://www.creative-tim.com/product/material-dashboard-react
- * Copyright 2020 Creative Tim (http://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
  */
 
 // ##############################
