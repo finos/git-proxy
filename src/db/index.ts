ced672de04f20ad4a003a1725176030c1fa1f1ec
Kris West <kristopher.west@natwest.com>
2025-05-22 17:51:59 +0100

    feat(key on repo url): apply proper typing to DB classes
    Typescript wasn't working on the DB classes due to their dependency imports with require.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,17 @@
-import { Repo } from './types';
+import { AuthorisedRepo } from '../config/types';
+import { PushQuery, Repo, Sink, User } from './types';
+import * as bcrypt from 'bcryptjs';
+import * as config from '../config';
+import * as mongo from './mongo';
+import * as neDb from './file';
+import { Action } from '../proxy/actions/Action';
+import MongoDBStore from 'connect-mongo';
 
-const bcrypt = require('bcryptjs');
-const config = require('../config');
-let sink: any;
+let sink: Sink;
 if (config.getDatabase().type === 'mongo') {
-  sink = require('./mongo');
+  sink = mongo;
 } else if (config.getDatabase().type === 'fs') {
-  sink = require('./file');
+  sink = neDb;
 }
 
 const isBlank = (str: string) => {
@@ -62,26 +67,29 @@ export const createUser = async (
   await sink.createUser(data);
 };
 
-export const createRepo = async (repo: Repo) => {
-  repo.name = repo.name.toLowerCase();
-  repo.users = {
-    canPush: [],
-    canAuthorise: [],
+export const createRepo = async (repo: AuthorisedRepo) => {
+  const toCreate = {
+    ...repo,
+    users: {
+      canPush: [],
+      canAuthorise: [],
+    },
   };
+  toCreate.name = repo.name.toLowerCase();
 
-  console.log(`creating new repo ${JSON.stringify(repo)}`);
+  console.log(`creating new repo ${JSON.stringify(toCreate)}`);
 
-  if (isBlank(repo.project)) {
+  if (isBlank(toCreate.project)) {
     throw new Error('Project name cannot be empty');
   }
-  if (isBlank(repo.name)) {
+  if (isBlank(toCreate.name)) {
     throw new Error('Repository name cannot be empty');
   }
-  if (isBlank(repo.url)) {
+  if (isBlank(toCreate.url)) {
     throw new Error('URL cannot be empty');
   }
 
-  return sink.createRepo(repo);
+  return sink.createRepo(toCreate) as Promise<Required<Repo>>;
 };
 
 export const getRepoByUrl = async (repoUrl: string) => {
@@ -135,7 +143,7 @@ export const canUserApproveRejectPush = async (id: string, user: string) => {
 
     const theRepo = await sink.getRepoByUrl(action.url);
 
-    if (theRepo.users.canAuthorise.includes(user)) {
+    if (theRepo?.users?.canAuthorise?.includes(user)) {
       console.log(`user ${user} can approve/reject for repo ${action.url}`);
       resolve(true);
     } else {
@@ -163,25 +171,31 @@ export const canUserCancelPush = async (id: string, user: string) => {
   });
 };
 
-export const {
-  authorise,
-  reject,
-  cancel,
-  getPushes,
-  writeAudit,
-  getPush,
-  deletePush,
-  findUser,
-  findUserByOIDC,
-  getUsers,
-  deleteUser,
-  updateUser,
-  getRepos,
-  getRepoById,
-  addUserCanPush,
-  addUserCanAuthorise,
-  removeUserCanAuthorise,
-  removeUserCanPush,
-  deleteRepo,
-  getSessionStore,
-} = sink;
+export const getSessionStore = (): MongoDBStore | null =>
+  sink.getSessionStore ? sink.getSessionStore() : null;
+export const getPushes = (query: PushQuery): Promise<Action[]> => sink.getPushes(query);
+export const writeAudit = (action: Action): Promise<void> => sink.writeAudit(action);
+export const getPush = (id: string): Promise<Action | null> => sink.getPush(id);
+export const deletePush = (id: string): Promise<void> => sink.deletePush(id);
+export const authorise = (id: string, attestation: any): Promise<{ message: string }> =>
+  sink.authorise(id, attestation);
+export const cancel = (id: string): Promise<{ message: string }> => sink.cancel(id);
+export const reject = (id: string): Promise<{ message: string }> => sink.reject(id);
+export const getRepos = (query?: object): Promise<Repo[]> => sink.getRepos(query);
+export const getRepo = (name: string): Promise<Repo | null> => sink.getRepo(name);
+// export const getRepoByUrl = (url: string): Promise<Repo | null> => sink.getRepoByUrl(url);
+export const getRepoById = (_id: string): Promise<Repo | null> => sink.getRepoById(_id);
+export const addUserCanPush = (_id: string, user: string): Promise<void> =>
+  sink.addUserCanPush(_id, user);
+export const addUserCanAuthorise = (_id: string, user: string): Promise<void> =>
+  sink.addUserCanAuthorise(_id, user);
+export const removeUserCanPush = (_id: string, user: string): Promise<void> =>
+  sink.removeUserCanPush(_id, user);
+export const removeUserCanAuthorise = (_id: string, user: string): Promise<void> =>
+  sink.removeUserCanAuthorise(_id, user);
+export const deleteRepo = (_id: string): Promise<void> => sink.deleteRepo(_id);
+export const findUser = (username: string): Promise<User | null> => sink.findUser(username);
+export const findUserByOIDC = (oidcId: string): Promise<User | null> => sink.findUserByOIDC(oidcId);
+export const getUsers = (query?: object): Promise<User[]> => sink.getUsers(query);
+export const deleteUser = (username: string): Promise<void> => sink.deleteUser(username);
+export const updateUser = (user: User): Promise<void> => sink.updateUser(user);
