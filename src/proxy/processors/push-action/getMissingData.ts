da3205adbc6d0a9c2d68e859b92cdd58541f81c8
Juan Escalada <juanescalada175@gmail.com>
2025-05-29 12:41:20 +0900

    refactor: getMissingData control flow

--- src/proxy/processors/push-action/getMissingData.ts
+++ src/proxy/processors/push-action/getMissingData.ts
@@ -7,40 +7,40 @@ import { EMPTY_COMMIT_HASH } from '../constants';
 const exec = async (req: any, action: Action): Promise<Action> => {
   const step = new Step('getMissingData');
 
+  if (action.commitData && action.commitData.length > 0) {
+    console.log('getMissingData', action);
+    return action;
+  }
+
+  console.log(`commitData not found, fetching missing commits from git...`);
+
   try {
-    if (!action.commitData) {
-      action.commitData = [];
-    }
+    const path = `${action.proxyGitPath}/${action.repoName}`;
+    const git = simpleGit(path);
+    const log = await git.log({ from: action.commitFrom, to: action.commitTo });
 
-    if (action.commitData.length === 0) {
-      console.log(`commitData not found, fetching missing commits from git...`);
-      const path = `${action.proxyGitPath}/${action.repoName}`;
-      const git = simpleGit(path);
-      const log = await git.log({ from: action.commitFrom, to: action.commitTo });
-
-      action.commitData = log.all.toReversed().map((entry, i, array) => {
-        const parent = i === 0 ? action.commitFrom : array[i - 1].hash;
-        const timestamp = Math.floor(new Date(entry.date).getTime() / 1000).toString();
-        return {
-          message: entry.message || '',
-          committer: entry.author_name || '',
-          tree: entry.hash || '',
-          parent: parent || '0'.repeat(40),
-          author: entry.author_name || '',
-          authorEmail: entry.author_email || '',
-          commitTimestamp: timestamp,
-        }
-      });
-      console.log(`Updated commitData:`, { commitData: action.commitData });
+    action.commitData = log.all.toReversed().map((entry, i, array) => {
+      const parent = i === 0 ? action.commitFrom : array[i - 1].hash;
+      const timestamp = Math.floor(new Date(entry.date).getTime() / 1000).toString();
+      return {
+        message: entry.message || '',
+        committer: entry.author_name || '',
+        tree: entry.hash || '',
+        parent: parent || '0'.repeat(40),
+        author: entry.author_name || '',
+        authorEmail: entry.author_email || '',
+        commitTimestamp: timestamp,
+      }
+    });
+    console.log(`Updated commitData:`, { commitData: action.commitData });
 
     if (action.commitFrom === EMPTY_COMMIT_HASH) {
-        action.commitFrom = action.commitData[action.commitData.length - 1].parent;
-      }
-      const user = action.commitData[action.commitData.length - 1].committer;
-      action.user = user;
-
-      return await validateUser(user, action, step);
+      action.commitFrom = action.commitData[action.commitData.length - 1].parent;
     }
+    const user = action.commitData[action.commitData.length - 1].committer;
+    action.user = user;
+
+    return await validateUser(user, action, step);
   } catch (e: any) {
     step.setError(e.toString('utf-8'));
   } finally {
