55429f5b67c95f8b6b3e9f0716f29914070f4581
Kris West <kristopher.west@natwest.com>
2025-06-12 15:56:20 +0100

    test: enable tests that weren't being run due to file name

--- test/testProxyRoute.test.js
+++ test/testProxyRoute.test.js
@@ -112,26 +112,26 @@ describe('proxy route helpers', () => {
       });
       expect(res).to.be.true;
     });
-  
+
     it('should return true for /info/refs?service=git-receive-pack with valid user-agent', () => {
       const res = validGitRequest('/info/refs?service=git-receive-pack', {
         'user-agent': 'git/1.9.1',
       });
       expect(res).to.be.true;
     });
-  
+
     it('should return false for /info/refs?service=git-upload-pack with missing user-agent', () => {
       const res = validGitRequest('/info/refs?service=git-upload-pack', {});
       expect(res).to.be.false;
     });
-  
+
     it('should return false for /info/refs?service=git-upload-pack with non-git user-agent', () => {
       const res = validGitRequest('/info/refs?service=git-upload-pack', {
         'user-agent': 'curl/7.79.1',
       });
       expect(res).to.be.false;
     });
-  
+
     it('should return true for /git-upload-pack with valid user-agent and accept', () => {
       const res = validGitRequest('/git-upload-pack', {
         'user-agent': 'git/2.40.0',
@@ -139,14 +139,14 @@ describe('proxy route helpers', () => {
       });
       expect(res).to.be.true;
     });
-  
+
     it('should return false for /git-upload-pack with missing accept header', () => {
       const res = validGitRequest('/git-upload-pack', {
         'user-agent': 'git/2.40.0',
       });
       expect(res).to.be.false;
     });
-  
+
     it('should return false for /git-upload-pack with wrong accept header', () => {
       const res = validGitRequest('/git-upload-pack', {
         'user-agent': 'git/2.40.0',
@@ -154,7 +154,7 @@ describe('proxy route helpers', () => {
       });
       expect(res).to.be.false;
     });
-  
+
     it('should return false for unknown paths', () => {
       const res = validGitRequest('/not-a-valid-git-path', {
         'user-agent': 'git/2.40.0',
@@ -169,32 +169,32 @@ describe('proxy route helpers', () => {
       const res = stripGitHubFromGitPath('/foo/bar.git/info/refs');
       expect(res).to.equal('/info/refs');
     });
-  
+
     it('should strip owner and repo from a valid GitHub-style path with 5 parts', () => {
       const res = stripGitHubFromGitPath('/foo/bar.git/git-upload-pack');
       expect(res).to.equal('/git-upload-pack');
     });
-  
+
     it('should return undefined for malformed path with too few segments', () => {
       const res = stripGitHubFromGitPath('/foo/bar.git');
       expect(res).to.be.undefined;
     });
-  
+
     it('should return undefined for malformed path with too many segments', () => {
       const res = stripGitHubFromGitPath('/foo/bar.git/extra/path/stuff');
       expect(res).to.be.undefined;
     });
-  
+
     it('should handle repo names that include dots correctly', () => {
       const res = stripGitHubFromGitPath('/foo/some.repo.git/info/refs');
       expect(res).to.equal('/info/refs');
     });
-  
+
     it('should not break if the path is just a slash', () => {
       const res = stripGitHubFromGitPath('/');
       expect(res).to.be.undefined;
     });
-  
+
     it('should not break if the path is empty', () => {
       const res = stripGitHubFromGitPath('');
       expect(res).to.be.undefined;
