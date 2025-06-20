662830c7cdaf446aeaa43f6114fc547f4e66af1c
Kris West <kristopher.west@natwest.com>
2025-06-20 10:25:59 +0100

    test: proxy route filter tests

--- test/testProxyRoute.test.js
+++ test/testProxyRoute.test.js
@@ -1,14 +1,15 @@
 const { handleMessage, validGitRequest, stripGitHubFromGitPath } = require('../src/proxy/routes');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+chai.should();
+const expect = chai.expect;
 const sinon = require('sinon');
 const express = require('express');
 const proxyRouter = require('../src/proxy/routes').router;
 const chain = require('../src/proxy/chain');
-
-chai.use(chaiHttp);
-chai.should();
-const expect = chai.expect;
+const proxyquire = require('proxyquire');
+const { Action, Step } = require('../src/proxy/actions');
 
 describe('proxy route filter middleware', () => {
   let app;
@@ -199,5 +200,118 @@ describe('proxy route helpers', () => {
       const res = stripGitHubFromGitPath('');
       expect(res).to.be.undefined;
     });
+  });
+});
+
+describe('proxy route filter', async () => {
+  let proxyRoutes;
+  let req;
+  let res;
+  let actionToReturn;
+  let executeChainStub;
+
+  beforeEach(async () => {
+    executeChainStub = sinon.stub();
+
+    // Re-import the proxy routes module and stub executeChain
+    proxyRoutes = proxyquire('../src/proxy/routes', {
+      '../chain': { executeChain: executeChainStub },
+    });
+
+    req = {
+      url: '/github.com/finos/git-proxy.git/info/refs?service=git-receive-pack',
+      headers: {
+        host: 'dummyHost',
+        'user-agent': 'git/dummy-git-client',
+        accept: 'application/x-git-receive-pack-request',
+      },
+    };
+    res = {
+      set: () => {},
+      status: () => {
+        return {
+          send: () => {},
+        };
+      },
+    };
+  });
+
+  it('should return false for push requests that should be blocked', async function () {
+    // mock the executeChain function
+    actionToReturn = new Action(
+      1234,
+      'dummy',
+      'dummy',
+      Date.now(),
+      '/github.com/finos/git-proxy.git',
+    );
+    const step = new Step('dummy', false, null, true, 'test block', null);
+    actionToReturn.addStep(step);
+    executeChainStub.returns(actionToReturn);
+    const result = await proxyRoutes.proxyFilter(req, res);
+    expect(result).to.be.false;
+  });
+
+  it('should return false for push requests that produced errors', async function () {
+    // mock the executeChain function
+    actionToReturn = new Action(
+      1234,
+      'dummy',
+      'dummy',
+      Date.now(),
+      '/github.com/finos/git-proxy.git',
+    );
+    const step = new Step('dummy', true, 'test error', false, null, null);
+    actionToReturn.addStep(step);
+    executeChainStub.returns(actionToReturn);
+    const result = await proxyRoutes.proxyFilter(req, res);
+    expect(result).to.be.false;
+  });
+
+  it('should return false for invalid push requests', async function () {
+    // mock the executeChain function
+    actionToReturn = new Action(
+      1234,
+      'dummy',
+      'dummy',
+      Date.now(),
+      '/github.com/finos/git-proxy.git',
+    );
+    const step = new Step('dummy', true, 'test error', false, null, null);
+    actionToReturn.addStep(step);
+    executeChainStub.returns(actionToReturn);
+
+    // create an invalid request
+    req = {
+      url: '/github.com/finos/git-proxy.git/invalidPath',
+      headers: {
+        host: 'dummyHost',
+        'user-agent': 'git/dummy-git-client',
+        accept: 'application/x-git-receive-pack-request',
+      },
+    };
+
+    const result = await proxyRoutes.proxyFilter(req, res);
+    expect(result).to.be.false;
+  });
+
+  it('should return true for push requests that are valid and pass the chain', async function () {
+    // mock the executeChain function
+    actionToReturn = new Action(
+      1234,
+      'dummy',
+      'dummy',
+      Date.now(),
+      '/github.com/finos/git-proxy.git',
+    );
+    const step = new Step('dummy', false, null, false, null, null);
+    actionToReturn.addStep(step);
+    executeChainStub.returns(actionToReturn);
+    const result = await proxyRoutes.proxyFilter(req, res);
+    expect(result).to.be.true;
+  });
+
+  afterEach(() => {
+    sinon.restore();
   });
 });
