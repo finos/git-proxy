74d5081a896e9f3437ab437c21000a7c4488489d
Jack Kelly <jack@control-plane.io>
2025-07-08 11:57:42 +0100

    fix: only trim trailing .git not any match
    Currently replacing .git even in the middle of valid repo names.
    This fix only trims from the back.

--- src/db/helper.ts
+++ src/db/helper.ts
@@ -3,3 +3,13 @@ export const toClass = function (obj: any, proto: any) {
   obj.__proto__ = proto;
   return obj;
 };
+
+export const trimTrailingDotGit = (str: string): string => {
+  const target = '.git';
+  if (str.endsWith(target)) {
+    // extract string from 0 to the end minus the length of target
+    return str.slice(0, -target.length);
+  }
+  return str;
+};
+
