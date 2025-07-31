5932714ddc48940d641bdb9a3dc7c88f137126c3
Kris West <kristopher.west@natwest.com>
2025-07-31 14:51:05 +0100

    test: fixing test failure post merging main

--- src/proxy/processors/push-action/checkUserPushPermission.ts
+++ src/proxy/processors/push-action/checkUserPushPermission.ts
@@ -5,27 +5,30 @@ import { trimTrailingDotGit } from '../../../db/helper';
 // Execute if the repo is approved
 const exec = async (req: any, action: Action): Promise<Action> => {
   const step = new Step('checkUserPushPermission');
-  const user = action.user;
+  const userEmail = action.userEmail;
 
-  if (!user) {
-    console.log('Action has no user set. This may be due to a fast-forward ref update. Deferring to getMissingData action.');
+  if (!userEmail) {
+    console.log(
+      'Action has no userEmail set. This may be due to a fast-forward ref update. Deferring to getMissingData action.',
+    );
     return action;
   }
 
-  return await validateUser(user, action, step);
+  return await validateUser(userEmail, action, step);
 };
 
 /**
  * Helper that validates the user's push permission.
  * This can be used by other actions that need it. For example, when the user is missing from the commit data,
  * validation is deferred to getMissingData, but the logic is the same.
- * @param {string} user The user to validate
+ * @param {string} userEmail The user email to validate
  * @param {Action} action The action object
  * @param {Step} step The step object
  * @return {Promise<Action>} The action object
  */
-const validateUser = async (user: string, action: Action, step: Step): Promise<Action> => { 
+const validateUser = async (userEmail: string, action: Action, step: Step): Promise<Action> => {
   const repoSplit = trimTrailingDotGit(action.repo.toLowerCase()).split('/');
+
   // we expect there to be exactly one / separating org/repoName
   if (repoSplit.length != 2) {
     step.setError('Server-side issue extracting repoName');
@@ -36,13 +39,8 @@ const validateUser = async (user: string, action: Action, step: Step): Promise<A
   const repoName = repoSplit[1];
   let isUserAllowed = false;
 
-  // n.b. action.user will be set to whatever the user had set in their user.name config in their git client.
-  // it is not necessarily the GitHub username. GitHub looks users by email address as should we
-  const userEmail = action.userEmail;
-  let username = 'unknown';
-
   // Find the user associated with this email address
-  const list = await getUsers({ email: action.userEmail });
+  const list = await getUsers({ email: userEmail });
 
   if (list.length > 1) {
     console.error(`Multiple users found with email address ${userEmail}, ending`);
@@ -59,18 +57,15 @@ const validateUser = async (user: string, action: Action, step: Step): Promise<A
   } else if (list.length == 0) {
     console.error(`No user with email address ${userEmail} found`);
   } else {
-    username = list[0].username;
-    isUserAllowed = await isUserPushAllowed(repoName, username);
+    isUserAllowed = await isUserPushAllowed(repoName, list[0].username);
   }
 
-  console.log(`User ${username} <${userEmail}> permission on Repo ${repoName} : ${isUserAllowed}`);
+  console.log(`User ${userEmail} permission on Repo ${repoName} : ${isUserAllowed}`);
 
   if (!isUserAllowed) {
     console.log('User not allowed to Push');
     step.error = true;
-    step.log(
-      `User ${username} <${userEmail}> is not allowed to push on repo ${action.repo}, ending`,
-    );
+    step.log(`User ${userEmail} is not allowed to push on repo ${action.repo}, ending`);
 
     step.setError(
       `Your push has been blocked (${action.userEmail} ` +
@@ -81,7 +76,7 @@ const validateUser = async (user: string, action: Action, step: Step): Promise<A
     return action;
   }
 
-  step.log(`User ${username} <${userEmail}> is allowed to push on repo ${action.repo}`);
+  step.log(`User ${userEmail} is allowed to push on repo ${action.repo}`);
   action.addStep(step);
   return action;
 };
