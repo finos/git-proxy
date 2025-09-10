767ca64d00d2b646c3df55b9bb19db85e7eca7b1
Juan Escalada <juanescalada175@gmail.com>
2025-09-10 11:35:05 +0900

    chore: restore fuzz test

--- test/processors/testCheckRepoInAuthList.test.js
+++ test/processors/testCheckRepoInAuthList.test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const sinon = require('sinon');
+const fc = require('fast-check');
 const actions = require('../../src/proxy/actions/Action');
 const processor = require('../../src/proxy/processors/push-action/checkRepoInAuthorisedList');
 const expect = chai.expect;
@@ -34,5 +35,18 @@ describe('Check a Repo is in the authorised list', async () => {
     expect(result.steps[0].logs[0]).to.eq(
       'checkRepoInAuthorisedList - repo thisproject/repo-is-not-ok is not in the authorised whitelist, ending',
     );
+  });
+
+  describe('fuzzing', () => {
+    it('should not crash on random repo names', async () => {
+      await fc.assert(
+        fc.asyncProperty(fc.string(), async (repoName) => {
+          const action = new actions.Action('123', 'type', 'get', 1234, repoName);
+          const result = await processor.exec(null, action);
+          expect(result.error).to.be.true;
+        }),
+        { numRuns: 1000 },
+      );
+    });
   });
 });
