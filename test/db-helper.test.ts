445b5de5356bdf69db3858850384a0368dc622f8
Juan Escalada <juanescalada175@gmail.com>
2025-09-19 22:05:56 +0900

    refactor(vitest): db-helper tests

--- test/db-helper.test.ts
+++ test/db-helper.test.ts
@@ -1,63 +1,63 @@
-const { expect } = require('chai');
-const { trimPrefixRefsHeads, trimTrailingDotGit } = require('../src/db/helper');
+import { describe, it, expect } from 'vitest';
+import { trimPrefixRefsHeads, trimTrailingDotGit } from '../src/db/helper';
 
 describe('db helpers', () => {
   describe('trimPrefixRefsHeads', () => {
     it('removes `refs/heads/`', () => {
       const res = trimPrefixRefsHeads('refs/heads/test');
-      expect(res).to.equal('test');
+      expect(res).toBe('test');
     });
 
     it('removes only one `refs/heads/`', () => {
       const res = trimPrefixRefsHeads('refs/heads/refs/heads/');
-      expect(res).to.equal('refs/heads/');
+      expect(res).toBe('refs/heads/');
     });
 
     it('removes only the first `refs/heads/`', () => {
       const res = trimPrefixRefsHeads('refs/heads/middle/refs/heads/end/refs/heads/');
-      expect(res).to.equal('middle/refs/heads/end/refs/heads/');
+      expect(res).toBe('middle/refs/heads/end/refs/heads/');
     });
 
     it('handles empty string', () => {
       const res = trimPrefixRefsHeads('');
-      expect(res).to.equal('');
+      expect(res).toBe('');
     });
 
     it("doesn't remove `refs/heads`", () => {
       const res = trimPrefixRefsHeads('refs/headstest');
-      expect(res).to.equal('refs/headstest');
+      expect(res).toBe('refs/headstest');
     });
 
     it("doesn't remove `/refs/heads/`", () => {
       const res = trimPrefixRefsHeads('/refs/heads/test');
-      expect(res).to.equal('/refs/heads/test');
+      expect(res).toBe('/refs/heads/test');
     });
   });
 
   describe('trimTrailingDotGit', () => {
     it('removes `.git`', () => {
       const res = trimTrailingDotGit('test.git');
-      expect(res).to.equal('test');
+      expect(res).toBe('test');
     });
 
     it('removes only one `.git`', () => {
       const res = trimTrailingDotGit('.git.git');
-      expect(res).to.equal('.git');
+      expect(res).toBe('.git');
     });
 
     it('removes only the last `.git`', () => {
       const res = trimTrailingDotGit('.git-middle.git-end.git');
-      expect(res).to.equal('.git-middle.git-end');
+      expect(res).toBe('.git-middle.git-end');
     });
 
     it('handles empty string', () => {
       const res = trimTrailingDotGit('');
-      expect(res).to.equal('');
+      expect(res).toBe('');
     });
 
     it("doesn't remove just `git`", () => {
       const res = trimTrailingDotGit('testgit');
-      expect(res).to.equal('testgit');
+      expect(res).toBe('testgit');
     });
   });
 });
