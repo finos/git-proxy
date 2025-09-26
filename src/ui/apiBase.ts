2d977c97cede29149294ac7779350db2dffc38cd
Andy Pols <andy@pols.co.uk>
2025-09-26 18:45:51 +0100

    chore: default to location.origin and not empty string

--- src/ui/apiBase.ts
+++ src/ui/apiBase.ts
@@ -3,7 +3,7 @@ const stripTrailingSlashes = (s: string) => s.replace(/\/+$/, '');
 /**
  * The base URL for API requests.
  *
- * Uses the `VITE_API_URI` environment variable if set, otherwise defaults to the current origin.
+ * Uses the `VITE_API_URI` environment variable if set, otherwise defaults to the origin of the current page.
  * @return {string} The base URL to use for API requests.
  */
 export const API_BASE = process.env.VITE_API_URI
