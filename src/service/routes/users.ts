73b43d68ff69d229be6657498c1119c2320fffe0
Juan Escalada <juanescalada175@gmail.com>
2025-09-28 14:46:48 +0900

    fix: users endpoint merge conflict

--- src/service/routes/users.ts
+++ src/service/routes/users.ts
@@ -3,24 +3,10 @@ const router = express.Router();
 
 import * as db from '../../db';
 import { toPublicUser } from './publicApi';
-import { UserQuery } from '../../db/types';
 
 router.get('/', async (req: Request, res: Response) => {
-  const query: Partial<UserQuery> = {};
-
-  console.log(`fetching users = query path =${JSON.stringify(req.query)}`);
-  for (const k in req.query) {
-    if (!k) continue;
-    if (k === 'limit' || k === 'skip') continue;
-
-    const rawValue = req.query[k];
-    let parsedValue: boolean | undefined;
-    if (rawValue === 'false') parsedValue = false;
-    if (rawValue === 'true') parsedValue = true;
-    query[k] = parsedValue ?? rawValue?.toString();
-  }
-
-  const users = await db.getUsers(query);
+  console.log('fetching users');
+  const users = await db.getUsers({});
   res.send(users.map(toPublicUser));
 });
 
