99d626fbf95134a0fa741380a3dba050168fbb0e
Andy Pols <andy@pols.co.uk>
2025-09-29 14:14:21 +0100

    chore: revert mistaken commit

--- src/ui/apiBase.ts
+++ src/ui/apiBase.ts
@@ -3,7 +3,7 @@ const stripTrailingSlashes = (s: string) => s.replace(/\/+$/, '');
 /**
  * The base URL for API requests.
  *
- * Uses the `VITE_API_URI` environment variable if set, otherwise defaults to the origin of the current page.
+ * Uses the `VITE_API_URI` environment variable if set, otherwise defaults to the current origin.
  * @return {string} The base URL to use for API requests.
  */
 export const API_BASE = process.env.VITE_API_URI
