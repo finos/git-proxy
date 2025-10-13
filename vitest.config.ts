52cfaab5a601d5dbf7f0283b0fb5ece1055a5c9d
Juan Escalada <juanescalada175@gmail.com>
2025-10-13 14:37:01 +0900

    chore: update vitest config to limit coverage check to API

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,5 +8,22 @@ export default defineConfig({
         singleFork: true, // Run all tests in a single process
       },
     },
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './coverage',
+      reporter: ['text', 'lcov'],
+      exclude: [
+        'dist',
+        'src/ui',
+        'src/contents',
+        'src/config/generated',
+        'website',
+        'packages',
+        'experimental',
+      ],
+      thresholds: {
+        lines: 80,
+      },
+    },
   },
 });
