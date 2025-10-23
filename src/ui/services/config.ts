5b6df0aa5a51205d8c936c4c3e1c9300a2b567b6
Juan Escalada <juanescalada175@gmail.com>
2025-10-23 13:06:16 +0900

    refactor(ts): src/ui/services/config and improve function names

---
+++ src/ui/services/config.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { API_BASE } from '../apiBase';
+import { FormQuestion } from '../views/PushDetails/components/AttestationForm';
+import { UIRouteAuth } from '../../config/generated/config';
+
+const setAttestationConfigData = async (setData: (data: FormQuestion[]) => void) => {
+  const url = new URL(`${API_BASE}/config/attestation`);
+  await axios(url.toString()).then((response) => {
+    setData(response.data.questions);
+  });
+};
+
+const setURLShortenerData = async (setData: (data: string) => void) => {
+  const url = new URL(`${API_BASE}/config/urlShortener`);
+  await axios(url.toString()).then((response) => {
+    setData(response.data);
+  });
+};
+
+const setEmailContactData = async (setData: (data: string) => void) => {
+  const url = new URL(`${API_BASE}/config/contactEmail`);
+  await axios(url.toString()).then((response) => {
+    setData(response.data);
+  });
+};
+
+const setUIRouteAuthData = async (setData: (data: UIRouteAuth) => void) => {
+  const url = new URL(`${API_BASE}/config/uiRouteAuth`);
+  await axios(url.toString()).then((response) => {
+    setData(response.data);
+  });
+};
+
+export { setAttestationConfigData, setURLShortenerData, setEmailContactData, setUIRouteAuthData };
