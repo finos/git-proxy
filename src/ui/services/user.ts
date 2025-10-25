4c2ddc27ccfba762ad2604114d5b20408af20da1
Andy Pols <andy@pols.co.uk>
2025-10-25 18:32:15 +0100

    chore: remove unused getUserLoggedIn

--- src/ui/services/user.ts
+++ src/ui/services/user.ts
@@ -80,29 +80,4 @@ const updateUser = async (data: UserData): Promise<void> => {
   }
 };
 
-const getUserLoggedIn = async (
-  setIsLoading: SetStateCallback<boolean>,
-  setIsAdmin: SetStateCallback<boolean>,
-  setIsError: SetStateCallback<boolean>,
-  setAuth: SetStateCallback<boolean>,
-): Promise<void> => {
-  try {
-    const response: AxiosResponse<UserData> = await axios(
-      `${API_BASE}/api/auth/me`,
-      getAxiosConfig(),
-    );
-    const data = response.data;
-    setIsLoading(false);
-    setIsAdmin(data.admin || false);
-  } catch (error) {
-    setIsLoading(false);
-    const axiosError = error as AxiosError;
-    if (axiosError.response?.status === 401) {
-      setAuth(false);
-    } else {
-      setIsError(true);
-    }
-  }
-};
-
-export { getUser, getUsers, updateUser, getUserLoggedIn };
+export { getUser, getUsers, updateUser };
