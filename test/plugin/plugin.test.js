d9576a042243dc7e9f599f2df4f500522ae9e762
Juan Escalada <juanescalada175@gmail.com>
2025-10-29 09:50:49 +0900

    chore: remove .skip in plugin tests

--- test/plugin/plugin.test.js
+++ test/plugin/plugin.test.js
@@ -22,42 +22,34 @@ describe('loading plugins from packages', function () {
     spawnSync('npm', ['install'], { cwd: testPackagePath, timeout: 5000 });
   });
 
-  it.skip(
-    'should load plugins that are the default export (module.exports = pluginObj)',
-    async function () {
-      const loader = new PluginLoader([join(testPackagePath, 'default-export.js')]);
-      await loader.load();
-      expect(loader.pushPlugins.length).to.equal(1);
-      expect(loader.pushPlugins.every((p) => isCompatiblePlugin(p))).to.be.true;
-      expect(loader.pushPlugins[0]).to.be.an.instanceOf(PushActionPlugin);
-    },
-  ).timeout(10000);
+  it('should load plugins that are the default export (module.exports = pluginObj)', async function () {
+    const loader = new PluginLoader([join(testPackagePath, 'default-export.js')]);
+    await loader.load();
+    expect(loader.pushPlugins.length).to.equal(1);
+    expect(loader.pushPlugins.every((p) => isCompatiblePlugin(p))).to.be.true;
+    expect(loader.pushPlugins.every((p) => isCompatiblePlugin(p, 'isGitProxyPushActionPlugin'))).to
+      .be.true;
+  }).timeout(10000);
 
-  it.skip(
-    'should load multiple plugins from a module that match the plugin class (module.exports = { pluginFoo, pluginBar })',
-    async function () {
-      const loader = new PluginLoader([join(testPackagePath, 'multiple-export.js')]);
-      await loader.load();
-      expect(loader.pushPlugins.length).to.equal(1);
-      expect(loader.pullPlugins.length).to.equal(1);
-      expect(loader.pushPlugins.every((p) => isCompatiblePlugin(p))).to.be.true;
-      expect(loader.pushPlugins.every((p) => isCompatiblePlugin(p, 'isGitProxyPushActionPlugin')))
-        .to.be.true;
-      expect(loader.pullPlugins.every((p) => isCompatiblePlugin(p, 'isGitProxyPullActionPlugin')))
-        .to.be.true;
-      expect(loader.pushPlugins[0]).to.be.instanceOf(PushActionPlugin);
-      expect(loader.pullPlugins[0]).to.be.instanceOf(PullActionPlugin);
-    },
-  ).timeout(10000);
+  it('should load multiple plugins from a module that match the plugin class (module.exports = { pluginFoo, pluginBar })', async function () {
+    const loader = new PluginLoader([join(testPackagePath, 'multiple-export.js')]);
+    await loader.load();
+    expect(loader.pushPlugins.length).to.equal(1);
+    expect(loader.pullPlugins.length).to.equal(1);
+    expect(loader.pushPlugins.every((p) => isCompatiblePlugin(p))).to.be.true;
+    expect(loader.pushPlugins.every((p) => isCompatiblePlugin(p, 'isGitProxyPushActionPlugin'))).to
+      .be.true;
+    expect(loader.pullPlugins.every((p) => isCompatiblePlugin(p, 'isGitProxyPullActionPlugin'))).to
+      .be.true;
+  }).timeout(10000);
 
-  it.skip('should load plugins that are subclassed from plugin classes', async function () {
+  it('should load plugins that are subclassed from plugin classes', async function () {
     const loader = new PluginLoader([join(testPackagePath, 'subclass.js')]);
     await loader.load();
     expect(loader.pushPlugins.length).to.equal(1);
     expect(loader.pushPlugins.every((p) => isCompatiblePlugin(p))).to.be.true;
     expect(loader.pushPlugins.every((p) => isCompatiblePlugin(p, 'isGitProxyPushActionPlugin'))).to
       .be.true;
-    expect(loader.pushPlugins[0]).to.be.instanceOf(PushActionPlugin);
   }).timeout(10000);
 
   it('should not load plugins that are not valid modules', async function () {
