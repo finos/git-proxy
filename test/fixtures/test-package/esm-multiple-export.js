4664c79eaec493d0bcf733b1ab7e381ea1e6bced
Juan Escalada <97265671+jescalada@users.noreply.github.com>
2025-11-03 13:20:15 +0900

    Update test/fixtures/test-package/esm-multiple-export.js
    Co-authored-by: Thomas Cooper <57812123+coopernetes@users.noreply.github.com>
    Signed-off-by: Juan Escalada <97265671+jescalada@users.noreply.github.com>

--- test/fixtures/test-package/esm-multiple-export.js
+++ test/fixtures/test-package/esm-multiple-export.js
@@ -10,4 +10,9 @@ export default {
     console.log('PullActionPlugin: ', action);
     return action;
   }),
+  baz: {
+    exec: (async (req, action) => {
+      console.log('not a real plugin object');
+    }),
+  },
 };
