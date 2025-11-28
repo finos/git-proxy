c22c8605eed630df1d12d79c5313df0418e76571
Juan Escalada <97265671+jescalada@users.noreply.github.com>
2025-11-28 14:10:29 +0000

    Update test/proxy.test.ts
    Co-authored-by: Kris West <kristopher.west@natwest.com>
    Signed-off-by: Juan Escalada <97265671+jescalada@users.noreply.github.com>

--- test/proxy.test.ts
+++ test/proxy.test.ts
@@ -12,7 +12,8 @@ import fs from 'fs';
 
   Related: skipped tests in testProxyRoute.test.ts - these have a race condition
   where either these or those tests fail depending on execution order
-  TODO: Find root cause of this error and fix it
+  TODO: Find root cause of this error and fix it 
+  https://github.com/finos/git-proxy/issues/1294
 */
 describe.skip('Proxy Module TLS Certificate Loading', () => {
   let proxyModule: any;
