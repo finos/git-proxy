9a8b2c66b12a48927f63d09f6a92b9b47d41b031
Kris West <kristopher.west@natwest.com>
2025-12-09 15:35:43 +0000

    fix: move neDB folder initialisation to occur on first use

--- src/db/file/helper.ts
+++ src/db/file/helper.ts
@@ -1,1 +1,7 @@
+import { existsSync, mkdirSync } from 'fs';
+
 export const getSessionStore = (): undefined => undefined;
+export const initializeFolders = () => {
+  if (!existsSync('./.data')) mkdirSync('./.data');
+  if (!existsSync('./.data/db')) mkdirSync('./.data/db');
+};
