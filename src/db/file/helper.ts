b4971c19cb253990bbe90649b945443b9d12bcf2
Kris West <kristopher.west@natwest.com>
2025-12-10 09:50:43 +0000

    Apply suggestions from code review
    Co-authored-by: Thomas Cooper <57812123+coopernetes@users.noreply.github.com>
    Signed-off-by: Kris West <kristopher.west@natwest.com>

--- src/db/file/helper.ts
+++ src/db/file/helper.ts
@@ -2,6 +2,5 @@ import { existsSync, mkdirSync } from 'fs';
 
 export const getSessionStore = (): undefined => undefined;
 export const initializeFolders = () => {
-  if (!existsSync('./.data')) mkdirSync('./.data');
-  if (!existsSync('./.data/db')) mkdirSync('./.data/db');
+  if (!existsSync('./.data/db')) mkdirSync('./.data/db', { recursive: true });
 };
