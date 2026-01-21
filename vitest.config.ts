37137f6ceffa5e83f2047765c26e5cfb5f6cc3e1
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2026-01-21 09:37:18 +0100

    feat: add mongodb-memory-server for cross-platform testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globalSetup: './test/globalSetup.ts',
     pool: 'forks',
     poolOptions: {
       forks: {
