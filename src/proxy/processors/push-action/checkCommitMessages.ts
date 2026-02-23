418f1ea426288fa4fedf6fc4374530aa94cc1fac
Richard Uriarte <ruriarte1998@outlook.com>
2026-02-23 11:46:49 -0500

    refactor: removed unnecessary comments

--- src/proxy/processors/push-action/checkCommitMessages.ts
+++ src/proxy/processors/push-action/checkCommitMessages.ts
@@ -7,13 +7,11 @@ const isMessageAllowed = (commitMessage: any): string | null => {
 
     // Commit message is empty, i.e. '', null or undefined
     if (!commitMessage) {
-      // console.log('No commit message included...');
       return 'No commit message included...';
     }
 
     // Validation for configured block pattern(s) check...
     if (typeof commitMessage !== 'string') {
-      // console.log('A non-string value has been captured for the commit message...');
       return 'A non-string value has been captured for the commit message...';
     }
 
@@ -36,11 +34,9 @@ const isMessageAllowed = (commitMessage: any): string | null => {
 
     // Commit message matches configured block pattern(s)
     if (literalMatches.length || patternMatches.length) {
-      // console.log('Commit message is blocked via configured literals/patterns...');
       return 'Commit message is blocked via configured literals/patterns...';
     }
   } catch (error) {
-    // console.log('Invalid regex pattern...');
     return 'Invalid regex pattern...';
   }
 
@@ -53,14 +49,11 @@ const exec = async (req: any, action: Action): Promise<Action> => {
 
   const uniqueCommitMessages = [...new Set(action.commitData?.map((commit) => commit.message))];
 
-  // const illegalMessages = uniqueCommitMessages.filter((message) => !isMessageAllowed(message));
   const illegalMessages = uniqueCommitMessages.filter(
     (message) => isMessageAllowed(message) !== null,
   );
 
   if (illegalMessages.length > 0) {
-    // console.log(`The following commit messages are illegal: ${illegalMessages}`);
-
     illegalMessages.forEach((message) => {
       const error = isMessageAllowed(message);
       step.log(
@@ -68,8 +61,6 @@ const exec = async (req: any, action: Action): Promise<Action> => {
       );
     });
 
-    // step.error = true;
-    // step.log(`The following commit messages are illegal: ${illegalMessages}`);
     step.setError(
       `\n\n\nYour push has been blocked.\nPlease ensure your commit message(s) does not contain sensitive information or URLs.\n\nThe following commit messages are illegal: ${JSON.stringify(illegalMessages)}\n\n`,
     );
@@ -78,7 +69,6 @@ const exec = async (req: any, action: Action): Promise<Action> => {
     return action;
   }
 
-  // console.log(`The following commit messages are legal: ${uniqueCommitMessages}`);
   step.log(`The following commit messages are legal: ${JSON.stringify(uniqueCommitMessages)}`);
   action.addStep(step);
   return action;
