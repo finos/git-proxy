d85b75c7b10bf45e9c3afdb5cd94374b3dac28cc
Andy Pols <andy@pols.co.uk>
2026-04-17 12:55:29 +0000

    chore: update tooling

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,9 @@
  * limitations under the License.
  */
 
-import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
 export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -29,7 +30,7 @@ export default ({ mode }: { mode: string }) => {
         ignored: ['**/.data/**', '**/.remote/**', '**/.tmp/**'],
       },
     },
-    plugins: [react()],
+    plugins: [tailwindcss(), react()],
     define: {
       'process.env': JSON.stringify(env),
     },
