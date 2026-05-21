aa12b520c733c4bb00e28e276b1e9bff21db6e4e
re-vlad <vladimir.z@re-partners.com>
2026-05-21 18:07:56 +0300

    fix: add Apache License headers to migration scripts

--- scripts/migrate/lib/config.js
+++ scripts/migrate/lib/config.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2026 GitProxy Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const path = require('path');
 
 const today = new Date().toISOString().split('T')[0];
@@ -6,7 +22,7 @@ module.exports = {
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017',
   dbName: process.env.DB_NAME || 'git_proxy',
   reportsDir: path.join(process.cwd(), 'reports', `${today}-migration`),
-  ensureReportsDir: function() {
+  ensureReportsDir: function () {
     const fs = require('fs');
     if (!fs.existsSync(this.reportsDir)) {
       fs.mkdirSync(this.reportsDir, { recursive: true });
