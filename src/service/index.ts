fbdb5b6785e5684f5aee51a85223b7fe8262f96f
Juan Escalada <97265671+jescalada@users.noreply.github.com>
2026-09-02 16:55:31 +0900

    Merge pull request #1703 from finos/fix-ui-publishing
    fix: broken UI build path
    (cherry picked from commit 9977bb0153023ef7c5dad38bb1c417b0765b729a)

--- src/service/index.ts
+++ src/service/index.ts
@@ -28,6 +28,7 @@ import { serverConfig } from '../config/env';
 import { Proxy } from '../proxy';
 import routes from './routes';
 import { configure } from './passport';
+import { UI_BUILD_PATH } from './urls';
 
 const limiter = rateLimit(config.getRateLimit());
 
@@ -129,7 +130,7 @@ async function createApp(proxy: Proxy): Promise<Express> {
   // configuration of passport is async
   // Before we can bind the routes - we need the passport strategy
   const passport = await configure();
-  const absBuildPath = path.join(__dirname, '../../build');
+  const absBuildPath = UI_BUILD_PATH;
   app.use(cors(corsOptions));
   app.set('trust proxy', 1);
   app.use(limiter);
@@ -168,9 +169,14 @@ async function createApp(proxy: Proxy): Promise<Express> {
   app.use('/', routes(proxy));
   app.use('/', express.static(absBuildPath));
   app.get('/*path', (_req, res) => {
-    res.sendFile(path.join(`${absBuildPath}/index.html`));
+    res.sendFile(path.join(absBuildPath, 'index.html'));
   });
 
+  if (!fs.existsSync(path.join(absBuildPath, 'index.html'))) {
+    console.error(
+      `UI build not found at ${absBuildPath}. The package may have been built or published incorrectly`,
+    );
+  }
   return app;
 }
 
