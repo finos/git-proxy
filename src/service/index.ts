61cca5bc8dc37a67ba5e41129722c6c2cf3f05d5
Juan Escalada <juanescalada175@gmail.com>
2026-09-07 23:43:32 +0900

    chore: add missing fs import

--- src/service/index.ts
+++ src/service/index.ts
@@ -21,6 +21,7 @@ import cors from 'cors';
 import path from 'path';
 import rateLimit from 'express-rate-limit';
 import lusca from 'lusca';
+import fs from 'fs';
 
 import * as config from '../config';
 import * as db from '../db';
